84ecee0a7c4d46d145905ac5927dcce39fb0909f
Osma Suominen <osma.suominen@helsinki.fi>
2018-03-13 16:47:44 +0200

    Implement HTTP backend. Fixes #48

---
+++ annif/backend/http.py
@@ -0,0 +1,16 @@
+"""HTTP/REST client backend that makes calles to a web service and returns the results"""
+
+
+import requests
+from annif.hit import AnalysisHit
+from . import backend
+
+
+class HTTPBackend(backend.AnnifBackend):
+    name = "http"
+
+    def analyze(self, text):
+        data = {'text': text, 'project': self.config['project']}
+        req = requests.post(self.config['endpoint'], data=data)
+        return [AnalysisHit(h['uri'], h['label'], h['score'])
+                for h in req.json()]
