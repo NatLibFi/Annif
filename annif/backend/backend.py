41b44fbdee3268c632cbd99c6355b0169915cdf5
Osma Suominen <osma.suominen@helsinki.fi>
2018-03-19 16:59:06 +0200

    Support backend specific data directories. Fixes #61

--- annif/backend/backend.py
+++ annif/backend/backend.py
@@ -1,6 +1,9 @@
 """Common functionality for backends."""
 
 import abc
+import os
+import os.path
+import annif
 
 
 class AnnifBackend(metaclass=abc.ABCMeta):
@@ -15,6 +18,19 @@ class AnnifBackend(metaclass=abc.ABCMeta):
         backend type."""
         self.backend_id = backend_id
         self.config = config
+        self._datadir = None
+
+    def _get_datadir(self):
+        """return the path of the directory where this backend can store its
+        data files"""
+        if self._datadir is None:
+            self._datadir = os.path.join(
+                annif.cxapp.app.config['DATADIR'],
+                'backends',
+                self.backend_id)
+            if not os.path.exists(self._datadir):
+                os.makedirs(self._datadir)
+        return self._datadir
 
     @abc.abstractmethod
     def analyze(self, text):
--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,6 +1,7 @@
 """Unit tests for backends in Annif"""
 
 import pytest
+import annif
 import annif.backend
 
 
@@ -27,3 +28,12 @@ def test_get_backend_dummy():
     assert result[0].uri == 'http://example.org/dummy'
     assert result[0].label == 'dummy'
     assert result[0].score == 1.0
+
+
+def test_project_datadir(tmpdir):
+    annif.cxapp.app.config['DATADIR'] = str(tmpdir)
+    dummy = annif.backend.get_backend('dummy')
+    datadir = dummy._get_datadir()
+    assert datadir == tmpdir.join('backends/dummy')
+    assert tmpdir.join('backends').exists()
+    assert tmpdir.join('backends/dummy').exists()
