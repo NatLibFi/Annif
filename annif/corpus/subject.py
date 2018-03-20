12995d7d42c26fbea7477f430238c4268a1cf3f6
Osma Suominen <osma.suominen@helsinki.fi>
2018-03-20 13:38:43 +0200

    First working version of TF-IDF backend. Fixes #46

--- annif/corpus/subject.py
+++ annif/corpus/subject.py
@@ -17,7 +17,6 @@ class SubjectDirectory:
     def __init__(self, path):
         self.path = path
         self._filenames = sorted(glob.glob(os.path.join(path, '*.txt')))
-        self._subjects = {}
 
     def __iter__(self):
         """Iterate through the subject directory, yielding Subject objects."""
@@ -27,10 +26,3 @@ class SubjectDirectory:
                 uri, label = subjfile.readline().strip().split(' ', 1)
                 text = ' '.join(subjfile.readlines())
                 yield Subject(uri, label, text)
-
-    def tokens(self, analyzer):
-        """Iterate through the subject directory, yielding lists of tokens
-        that are derived from subjects using the given analyzer."""
-
-        for subject in self:
-            yield analyzer.tokenize_words(subject.text)
