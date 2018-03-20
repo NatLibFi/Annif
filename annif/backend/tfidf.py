2fd4d8f78cf30900295c37f7930b434059aaa415
Osma Suominen <osma.suominen@helsinki.fi>
2018-03-20 09:29:36 +0200

    save dictionary as an atomic operation, to avoid race conditions

--- annif/backend/tfidf.py
+++ annif/backend/tfidf.py
@@ -1,7 +1,9 @@
 """Backend that returns most similar subjects based on similarity in sparse
 TF-IDF normalized bag-of-words vector space"""
 
+import os
 import os.path
+import tempfile
 import gensim.corpora
 from . import backend
 
@@ -9,10 +11,16 @@ from . import backend
 class TFIDFBackend(backend.AnnifBackend):
     name = "tfidf"
 
+    def _atomic_save(self, obj, dirname, filename):
+        tempfd, tempfilename = tempfile.mkstemp(prefix=filename, dir=dirname)
+        os.close(tempfd)
+        obj.save(tempfilename)
+        os.rename(tempfilename, os.path.join(dirname, filename))
+
     def load_subjects(self, subjects, analyzer):
         corpus = subjects.tokens(analyzer)
         dictionary = gensim.corpora.Dictionary(corpus)
-        dictionary.save(os.path.join(self._get_datadir(), 'dictionary'))
+        self._atomic_save(dictionary, self._get_datadir(), 'dictionary')
 
     def analyze(self, text):
         return []  # TODO
