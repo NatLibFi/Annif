0bef922521018cfb0110261b75458edaab323fb6
Osma Suominen <osma.suominen@helsinki.fi>
2018-03-22 10:57:33 +0200

    use a session-scoped temp directory to avoid loading tf-idf index twice

--- tests/test_backend_tfidf.py
+++ tests/test_backend_tfidf.py
@@ -4,10 +4,16 @@ import annif
 import annif.backend
 import annif.corpus
 import os.path
+import pytest
 
 
-def test_tfidf_load_subjects(tmpdir):
-    annif.cxapp.app.config['DATADIR'] = str(tmpdir)
+@pytest.fixture(scope='session')
+def datadir(tmpdir_factory):
+    return tmpdir_factory.mktemp('data')
+
+
+def test_tfidf_load_subjects(datadir):
+    annif.cxapp.app.config['DATADIR'] = str(datadir)
     tfidf_type = annif.backend.get_backend_type("tfidf")
     tfidf = tfidf_type(
         backend_id='tfidf',
@@ -24,30 +30,22 @@ def test_tfidf_load_subjects(tmpdir):
     assert len(tfidf._dictionary) > 0
     assert tfidf._tfidf is not None
     assert len(tfidf._index) > 0
-    assert tmpdir.join('backends/tfidf/subjects').exists()
-    assert tmpdir.join('backends/tfidf/subjects').size() > 0
-    assert tmpdir.join('backends/tfidf/dictionary').exists()
-    assert tmpdir.join('backends/tfidf/dictionary').size() > 0
-    assert tmpdir.join('backends/tfidf/tfidf').exists()
-    assert tmpdir.join('backends/tfidf/tfidf').size() > 0
-    assert tmpdir.join('backends/tfidf/index').exists()
-    assert tmpdir.join('backends/tfidf/index').size() > 0
+    assert datadir.join('backends/tfidf/subjects').exists()
+    assert datadir.join('backends/tfidf/subjects').size() > 0
+    assert datadir.join('backends/tfidf/dictionary').exists()
+    assert datadir.join('backends/tfidf/dictionary').size() > 0
+    assert datadir.join('backends/tfidf/tfidf').exists()
+    assert datadir.join('backends/tfidf/tfidf').size() > 0
+    assert datadir.join('backends/tfidf/index').exists()
+    assert datadir.join('backends/tfidf/index').size() > 0
 
 
-def test_tfidf_analyze(tmpdir):
-    annif.cxapp.app.config['DATADIR'] = str(tmpdir)
+def test_tfidf_analyze(datadir):
+    annif.cxapp.app.config['DATADIR'] = str(datadir)
     tfidf_type = annif.backend.get_backend_type("tfidf")
     tfidf = tfidf_type(
         backend_id='tfidf',
         params={'analyzer': 'snowball(finnish)', 'chunksize': 1, 'limit': 10})
-
-    subjdir = os.path.join(
-        os.path.dirname(__file__),
-        'corpora',
-        'archaeology',
-        'subjects')
-    subjects = annif.corpus.SubjectDirectory(subjdir)
-    tfidf.load_subjects(subjects)
 
     results = tfidf.analyze("""Arkeologiaa sanotaan joskus myös
         muinaistutkimukseksi tai muinaistieteeksi. Se on humanistinen tiede
