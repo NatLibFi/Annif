ba24e9f75da40c72080fef01ea795d08612c94e9
Osma Suominen <osma.suominen@helsinki.fi>
2018-03-22 14:43:04 +0200

    switch to click.echo() instead of print statements in CLI

--- annif/cli.py
+++ annif/cli.py
@@ -17,7 +17,7 @@ def get_project(project_id):
     try:
         return annif.project.get_project(project_id)
     except ValueError:
-        print("No projects found with id \'{0}\'.".format(project_id))
+        click.echo("No projects found with id \'{0}\'.".format(project_id))
         sys.exit(1)
 
 
@@ -31,13 +31,12 @@ def run_list_projects():
 
     template = "{0: <15}{1: <15}\n"
 
-    formatted = template.format("Project ID", "Language")
-    formatted += str("-" * len(formatted) + "\n")
+    header = template.format("Project ID", "Language")
+    click.echo(header)
+    click.echo("-" * len(header))
 
     for proj in annif.project.get_projects().values():
-        formatted += template.format(proj.project_id, proj.language)
-
-    print(formatted)
+        click.echo(template.format(proj.project_id, proj.language))
 
 
 @annif.cxapp.app.cli.command('show-project')
@@ -56,12 +55,10 @@ def run_show_project(project_id):
 
     proj = get_project(project_id)
 
-    formatted = ""
     template = "{0:<15}{1}\n"
 
-    formatted = template.format('Project ID:', proj.project_id)
-    formatted += template.format('Language:', proj.language)
-    print(formatted)
+    click.echo(template.format('Project ID:', proj.project_id))
+    click.echo(template.format('Language:', proj.language))
 
 
 @annif.cxapp.app.cli.command('load')
@@ -76,28 +73,28 @@ def run_load(project_id, directory):
 @annif.cxapp.app.cli.command('list-subjects')
 @click.argument('project_id')
 def run_list_subjects():
-    print("TODO")
+    click.echo("TODO")
 
 
 @annif.cxapp.app.cli.command('show-subject')
 @click.argument('project_id')
 @click.argument('subject_id')
 def run_show_subject(project_id, subject_id):
-    print("TODO")
+    click.echo("TODO")
 
 
 @annif.cxapp.app.cli.command('create-subject')
 @click.argument('project_id')
 @click.argument('subject_id')
 def run_create_subject(project_id, subject_id):
-    print("TODO")
+    click.echo("TODO")
 
 
 @annif.cxapp.app.cli.command('drop-subject')
 @click.argument('project_id')
 @click.argument('subject_id')
 def run_drop_subject(project_id, subject_id):
-    print("TODO")
+    click.echo("TODO")
 
 
 @annif.cxapp.app.cli.command('analyze')
@@ -114,7 +111,7 @@ def run_analyze(project_id, limit, threshold):
     text = sys.stdin.read()
     hits = project.analyze(text, limit, threshold)
     for hit in hits:
-        print("{}\t<{}>\t{}".format(hit.score, hit.uri, hit.label))
+        click.echo("{}\t<{}>\t{}".format(hit.score, hit.uri, hit.label))
 
 
 @annif.cxapp.app.cli.command('eval')
@@ -138,7 +135,7 @@ def run_eval(project_id, subject_file, limit, threshold):
 
     template = "{0:<10}\t{1}"
     for metric, result in annif.eval.evaluate_hits(hits, gold_subjects):
-        print(template.format(metric + ":", result))
+        click.echo(template.format(metric + ":", result))
 
 
 @annif.cxapp.app.cli.command('evaldir')
@@ -159,7 +156,6 @@ def run_evaldir(project_id, directory, limit, threshold):
     measures = collections.OrderedDict()
     for docfilename, subjectfilename in annif.corpus.DocumentDirectory(
             directory, require_subjects=True):
-        print("evaluating", docfilename, subjectfilename)
         with open(docfilename) as docfile:
             text = docfile.read()
         hits = project.analyze(text, limit, threshold)
@@ -172,4 +168,4 @@ def run_evaldir(project_id, directory, limit, threshold):
 
     template = "{0:<10}\t{1}"
     for metric, results in measures.items():
-        print(template.format(metric + ":", statistics.mean(results)))
+        click.echo(template.format(metric + ":", statistics.mean(results)))
