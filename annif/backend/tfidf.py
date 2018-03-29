89843f8fdc2b121d4a558192bcd0796ff69d05eb
Osma Suominen <osma.suominen@helsinki.fi>
2018-03-29 13:16:16 +0300

    Move SubjectIndex from TF-IDF backend to annif.corpus module so it's available to all backends

--- annif/backend/tfidf.py
+++ annif/backend/tfidf.py
@@ -32,47 +32,6 @@ class VectorCorpus:
                 self.analyzer.tokenize_words(subject.text))
 
 
-class SubjectIndex:
-    """A class that remembers the associations between integers subject IDs
-    and their URIs and labels."""
-
-    def __init__(self, corpus):
-        """Initialize the subject index from a subject corpus."""
-        self._uris = []
-        self._labels = []
-        for subject_id, subject in enumerate(corpus):
-            self._uris.append(subject.uri)
-            self._labels.append(subject.label)
-
-    def __len__(self):
-        return len(self._uris)
-
-    def __getitem__(self, subject_id):
-        return (self._uris[subject_id], self._labels[subject_id])
-
-    def save(self, path):
-        """Save this subject index into a file."""
-
-        with open(path, 'w') as subjfile:
-            for subject_id in range(len(self)):
-                line = "<{}>\t{}".format(
-                    self._uris[subject_id], self._labels[subject_id])
-                print(line, file=subjfile)
-
-    @classmethod
-    def load(cls, path):
-        """Load a subject index from a file and return it."""
-
-        def file_as_corpus(path):
-            with open(path) as subjfile:
-                for line in subjfile:
-                    uri, label = line.strip().split(None, 1)
-                    uri = uri[1:-1]
-                    yield annif.corpus.Subject(uri, label, None)
-
-        return cls(file_as_corpus(path))
-
-
 class TFIDFBackend(backend.AnnifBackend):
     name = "tfidf"
 
@@ -99,7 +58,7 @@ class TFIDFBackend(backend.AnnifBackend):
         if self._subjects is None:
             path = os.path.join(self._get_datadir(), 'subjects')
             self.debug('loading subjects from {}'.format(path))
-            self._subjects = SubjectIndex.load(path)
+            self._subjects = annif.corpus.SubjectIndex.load(path)
 
     def _initialize_dictionary(self):
         if self._dictionary is None:
@@ -127,7 +86,7 @@ class TFIDFBackend(backend.AnnifBackend):
 
     def load_subjects(self, subjects, project):
         self.info('Backend {}: creating subject index'.format(self.backend_id))
-        self._subjects = SubjectIndex(subjects)
+        self._subjects = annif.corpus.SubjectIndex(subjects)
         self._atomic_save(self._subjects, self._get_datadir(), 'subjects')
         self.info('creating dictionary')
         self._dictionary = gensim.corpora.Dictionary(
--- annif/corpus/subject.py
+++ annif/corpus/subject.py
@@ -26,3 +26,44 @@ class SubjectDirectory:
                 uri, label = subjfile.readline().strip().split(' ', 1)
                 text = ' '.join(subjfile.readlines())
                 yield Subject(uri, label, text)
+
+
+class SubjectIndex:
+    """A class that remembers the associations between integers subject IDs
+    and their URIs and labels."""
+
+    def __init__(self, corpus):
+        """Initialize the subject index from a subject corpus."""
+        self._uris = []
+        self._labels = []
+        for subject_id, subject in enumerate(corpus):
+            self._uris.append(subject.uri)
+            self._labels.append(subject.label)
+
+    def __len__(self):
+        return len(self._uris)
+
+    def __getitem__(self, subject_id):
+        return (self._uris[subject_id], self._labels[subject_id])
+
+    def save(self, path):
+        """Save this subject index into a file."""
+
+        with open(path, 'w') as subjfile:
+            for subject_id in range(len(self)):
+                line = "<{}>\t{}".format(
+                    self._uris[subject_id], self._labels[subject_id])
+                print(line, file=subjfile)
+
+    @classmethod
+    def load(cls, path):
+        """Load a subject index from a file and return it."""
+
+        def file_as_corpus(path):
+            with open(path) as subjfile:
+                for line in subjfile:
+                    uri, label = line.strip().split(None, 1)
+                    uri = uri[1:-1]
+                    yield Subject(uri, label, None)
+
+        return cls(file_as_corpus(path))
