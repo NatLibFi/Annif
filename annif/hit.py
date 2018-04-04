470dcb485ff91de8d6e2cf22793bc5b66fd2aecd
Osma Suominen <osma.suominen@helsinki.fi>
2018-04-04 10:42:09 +0300

    add unit tests for HitFilter and default to not limiting number of results

--- annif/hit.py
+++ annif/hit.py
@@ -11,13 +11,15 @@ class HitFilter:
     The original hits are given in the constructor along with parameters
     used for filtering. The collection can then be iterated."""
 
-    def __init__(self, hits, limit=1000, threshold=0.0):
+    def __init__(self, hits, limit=None, threshold=0.0):
         self._hits = hits
         self._limit = limit
         self._threshold = threshold
 
     def __iter__(self):
         hits = sorted(self._hits, key=lambda hit: hit.score, reverse=True)
-        for hit in hits[:self._limit]:
+        if self._limit is not None:
+            hits = hits[:self._limit]
+        for hit in hits:
             if hit.score >= self._threshold and hit.score > 0.0:
                 yield hit
