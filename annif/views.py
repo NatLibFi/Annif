b3eb111a49a2062f35c9d8c1812cba67091f944c
Osma Suominen <osma.suominen@helsinki.fi>
2018-04-09 13:58:16 +0300

    rename test UI template to avoid clobbering Swagger UI

--- annif/views.py
+++ annif/views.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 bp = Blueprint('app', __name__)
 
 
-@bp.route('/')
-def index():
-    return render_template('index.html')
+@bp.route('/test')
+def test():
+    return render_template('test.html')
