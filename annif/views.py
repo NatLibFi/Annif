2d643f77741adf3944cf753fa3323e6533b49f6a
Osma Suominen <osma.suominen@helsinki.fi>
2018-04-09 13:59:47 +0300

    adjust naming

--- annif/views.py
+++ annif/views.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 bp = Blueprint('app', __name__)
 
 
-@bp.route('/test')
-def test():
-    return render_template('test.html')
+@bp.route('/')
+def root():
+    return render_template('root.html')
