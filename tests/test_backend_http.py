308177fb834a9029d8a36b19e107e805c921c924
Osma Suominen <osma.suominen@helsinki.fi>
2018-04-13 16:03:29 +0300

    Add unit test for HTTP backend result with results field

--- tests/test_backend_http.py
+++ tests/test_backend_http.py
@@ -27,6 +27,29 @@ def test_http_analyze(app):
         assert result[0].score == 1.0
 
 
+def test_http_analyze_with_results(app):
+    with unittest.mock.patch('requests.post') as mock_request:
+        # create a mock response whose .json() method returns the list that we
+        # define here
+        mock_response = unittest.mock.Mock()
+        mock_response.json.return_value = {'results': [
+            {'uri': 'http://example.org/http', 'label': 'http', 'score': 1.0}]}
+        mock_request.return_value = mock_response
+
+        http_type = annif.backend.get_backend_type("http")
+        http = http_type(
+            backend_id='http',
+            params={
+                'endpoint': 'http://api.example.org/dummy/analyze',
+            },
+            datadir=app.config['DATADIR'])
+        result = http.analyze('this is some text', project=None)
+        assert len(result) == 1
+        assert result[0].uri == 'http://example.org/http'
+        assert result[0].label == 'http'
+        assert result[0].score == 1.0
+
+
 def test_http_analyze_zero_score(app):
     with unittest.mock.patch('requests.post') as mock_request:
         # create a mock response whose .json() method returns the list that we
