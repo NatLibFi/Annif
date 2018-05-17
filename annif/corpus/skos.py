1d76df990180e3be3039e8f44d8338462977c3fa
Osma Suominen <osma.suominen@helsinki.fi>
2018-05-17 16:42:42 +0300

    refactor: move helper function outside method

--- annif/corpus/skos.py
+++ annif/corpus/skos.py
@@ -6,6 +6,20 @@ from rdflib.namespace import SKOS, RDF, OWL
 from .subject import SubjectIndex, Subject
 
 
+def skos_file_as_corpus(path, language):
+    graph = rdflib.Graph()
+    format = rdflib.util.guess_format(path)
+    graph.load(path, format=format)
+    for concept in graph.subjects(RDF.type, SKOS.Concept):
+        if (concept, OWL.deprecated, rdflib.Literal(True)) in graph:
+            continue
+        labels = graph.preferredLabel(concept, lang=language)
+        if len(labels) == 0:
+            continue
+        label = str(labels[0][1])
+        yield Subject(uri=str(concept), label=label, text=None)
+
+
 class SubjectIndexSKOS (SubjectIndex):
     """A subject index that uses SKOS files instead of TSV files"""
 
@@ -20,18 +34,5 @@ class SubjectIndexSKOS (SubjectIndex):
     @classmethod
     def load(cls, path, language):
         """Load subjects from a SKOS file and return a subject index."""
-
-        def skos_file_as_corpus(path, language):
-            graph = rdflib.Graph()
-            format = rdflib.util.guess_format(path)
-            graph.load(path, format=format)
-            for concept in graph.subjects(RDF.type, SKOS.Concept):
-                if (concept, OWL.deprecated, rdflib.Literal(True)) in graph:
-                    continue
-                labels = graph.preferredLabel(concept, lang=language)
-                if len(labels) == 0:
-                    continue
-                label = str(labels[0][1])
-                yield Subject(uri=str(concept), label=label, text=None)
 
         return cls(skos_file_as_corpus(path, language))
