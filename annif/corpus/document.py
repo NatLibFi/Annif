ecec1f872af55c0f8349724eaccf57b43ce3fd56
Osma Suominen <osma.suominen@helsinki.fi>
2018-05-21 17:47:56 +0300

    refactor: move DocumentFile and DocumentDirectory into the same module

--- annif/corpus/document.py
+++ annif/corpus/document.py
@@ -1,9 +1,9 @@
-"""A directory of files as a full text document corpus"""
-
+"""Clases for supporting document corpora"""
 
 import glob
 import os.path
 import re
+import annif.util
 
 
 class DocumentDirectory:
@@ -29,3 +29,21 @@ class DocumentDirectory:
                 continue
             if not self.require_subjects:
                 yield (filename, None)
+
+
+class DocumentFile:
+    """A TSV file as a corpus of documents with subjects"""
+
+    def __init__(self, path):
+        self.path = path
+
+    def __iter__(self):
+        """Iterate through the file, yielding tuples of (text, subjects) where
+        subjects is a list of concept URIs."""
+
+        with open(self.path) as tsvfile:
+            for line in tsvfile:
+                text, uris = line.split('\t', maxsplit=1)
+                subjects = [annif.util.cleanup_uri(uri)
+                            for uri in uris.split()]
+                yield (text, subjects)
