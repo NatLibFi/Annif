92f598f268cbc60c912a4087ff4bba30ca2ef6e5
Osma Suominen <osma.suominen@helsinki.fi>
2018-05-28 10:58:07 +0300

    Support loading multiple document corpora in a single operation. Fixes #149

--- annif/corpus/__init__.py
+++ annif/corpus/__init__.py
@@ -5,3 +5,4 @@ from .document import DocumentDirectory, DocumentFile
 from .subject import Subject, SubjectDirectory, SubjectFileTSV
 from .subject import SubjectIndex, SubjectSet
 from .skos import SubjectFileSKOS
+from .combine import CombinedCorpus
