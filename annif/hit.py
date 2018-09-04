5ca6f91252fdc392decf952793ac36612e5f31f1
Osma Suominen <osma.suominen@helsinki.fi>
2018-09-04 14:02:56 +0300

    Ability to represent AnalysisHits as vector

--- annif/hit.py
+++ annif/hit.py
@@ -33,3 +33,13 @@ class AnalysisHits:
 
     def __getitem__(self, idx):
         return self._hits[idx]
+
+    def as_vector(self, subject_index):
+        """Return the hits as a one-dimensional vector of scores, using a
+           subject index as the source of subjects."""
+        score_by_uri = {}
+        for hit in self._hits:
+            score_by_uri[hit.uri] = hit.score
+        vector = [score_by_uri.get(uri, 0.0)
+                  for uri, label in subject_index]
+        return vector
