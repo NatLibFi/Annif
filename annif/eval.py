f1fef12f4ca7546b93025ef6ee0c077c037c1dce
Osma Suominen <osma.suominen@helsinki.fi>
2018-09-26 14:38:30 +0300

    Convert the remaining metrics to use vectors based on SubjectIndex

--- annif/eval.py
+++ annif/eval.py
@@ -4,7 +4,6 @@ import collections
 import statistics
 import warnings
 import numpy as np
-from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.metrics import precision_score, recall_score, f1_score
 from sklearn.metrics import label_ranking_average_precision_score
 
@@ -80,62 +79,40 @@ class EvaluationBatch:
     def evaluate(self, hits, gold_subjects):
         self._samples.append((hits, gold_subjects))
 
-    def _transform_sample(self, sample):
-        """transform a single document (sample) with predicted and gold
-           standard subjects into either sequences of URIs (if available) or
-           sequences of labels"""
-        hits, gold_subjects = sample
-        if gold_subjects.has_uris():
-            selected = [hit.uri for hit in hits]
-            gold_set = gold_subjects.subject_uris
-        else:
-            selected = [hit.label for hit in hits]
-            gold_set = gold_subjects.subject_labels
-        return (selected, gold_set)
-
     def results(self):
         """evaluate a set of selected subjects against a gold standard using
         different metrics"""
 
-        transformed_samples = [self._transform_sample(sample)
-                               for sample in self._samples]
-        selected, relevant = zip(*transformed_samples)
-
-        mlb = MultiLabelBinarizer()
-        mlb.fit(list(selected) + list(relevant))
-        y_true = mlb.transform(relevant)
-        y_pred = mlb.transform(selected)
-
-        y_true2 = np.array([gold_subjects.as_vector(self._subject_index)
-                            for hits, gold_subjects in self._samples])
-
-        y_scores = np.array([hits.as_vector(self._subject_index)
-                             for hits, gold_subjects in self._samples])
+        y_true = np.array([gold_subjects.as_vector(self._subject_index)
+                           for hits, gold_subjects in self._samples])
+        y_pred = np.array([hits.as_vector(self._subject_index)
+                           for hits, gold_subjects in self._samples])
+        y_pred_binary = y_pred > 0.0
 
         with warnings.catch_warnings():
             warnings.simplefilter('ignore')
 
             results = collections.OrderedDict([
                 ('Precision (per document average)',
-                 precision_score(y_true, y_pred, average='samples')),
+                 precision_score(y_true, y_pred_binary, average='samples')),
                 ('Recall (per document average)',
-                 recall_score(y_true, y_pred, average='samples')),
+                 recall_score(y_true, y_pred_binary, average='samples')),
                 ('F1 score (per document average)',
-                 f1_score(y_true, y_pred, average='samples')),
-                ('NDCG', ndcg_score(y_true2, y_scores)),
-                ('NDCG@5', ndcg_score(y_true2, y_scores, limit=5)),
-                ('NDCG@10', ndcg_score(y_true2, y_scores, limit=10)),
+                 f1_score(y_true, y_pred_binary, average='samples')),
+                ('NDCG', ndcg_score(y_true, y_pred)),
+                ('NDCG@5', ndcg_score(y_true, y_pred, limit=5)),
+                ('NDCG@10', ndcg_score(y_true, y_pred, limit=10)),
                 ('Precision@1 (per document average)',
-                 precision_at_k_score(y_true2, y_scores, limit=1)),
+                 precision_at_k_score(y_true, y_pred, limit=1)),
                 ('Precision@3 (per document average)',
-                 precision_at_k_score(y_true2, y_scores, limit=3)),
+                 precision_at_k_score(y_true, y_pred, limit=3)),
                 ('Precision@5 (per document average)',
-                 precision_at_k_score(y_true2, y_scores, limit=5)),
+                 precision_at_k_score(y_true, y_pred, limit=5)),
                 ('Label ranking average precision',
-                 label_ranking_average_precision_score(y_true2, y_scores)),
-                ('True positives', true_positives(y_true, y_pred)),
-                ('False positives', false_positives(y_true, y_pred)),
-                ('False negatives', false_negatives(y_true, y_pred))
+                 label_ranking_average_precision_score(y_true, y_pred)),
+                ('True positives', true_positives(y_true, y_pred_binary)),
+                ('False positives', false_positives(y_true, y_pred_binary)),
+                ('False negatives', false_negatives(y_true, y_pred_binary))
             ])
 
         return results
