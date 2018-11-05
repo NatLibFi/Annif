843a14a5d76dc021cb67384ea8a38d272edc989a
Osma Suominen <osma.suominen@helsinki.fi>
2018-11-05 17:10:04 +0200

    Initial code which lazily converts between vectors and lists-of-hits. Part of #188

--- annif/hit.py
+++ annif/hit.py
@@ -27,8 +27,14 @@ class HitFilter:
 class AnalysisResult:
     """A sequence of hits returned by an analysis operation."""
 
-    def __init__(self, hits):
-        self._hits = [hit for hit in hits if hit.score > 0.0]
+    def __init__(self, hits=None, vector=None, limit=None, subject_index=None):
+        if hits is not None:
+            self._hits = [hit for hit in hits if hit.score > 0.0]
+        else:
+            self._hits = None
+        self._vector = vector
+        self._limit = limit
+        self._subject_index = subject_index
 
     @classmethod
     def from_vector(cls, vector, limit, subject_index):
@@ -36,14 +42,20 @@ class AnalysisResult:
         where the indexes match the given subject index. Keep only the
         number of results specified by the limit parameter."""
 
-        top_scores = sorted(enumerate(vector),
+        return AnalysisResult(
+            vector=vector,
+            limit=limit,
+            subject_index=subject_index)
+
+    def _vector_to_hits(self):
+        top_scores = sorted(enumerate(self._vector),
                             key=lambda id_score: id_score[1],
                             reverse=True)
         hits = []
-        for subject_id, score in top_scores[:limit]:
+        for subject_id, score in top_scores[:self._limit]:
             if score <= 0.0:
                 continue
-            subject = subject_index[subject_id]
+            subject = self._subject_index[subject_id]
             hits.append(
                 AnalysisHit(
                     uri=subject[0],
@@ -51,18 +63,25 @@ class AnalysisResult:
                     score=score))
         return AnalysisResult(hits)
 
+    @property
+    def hits(self):
+        if self._hits is None:
+            self._hits = self._vector_to_hits()
+        return self._hits
+
     def __len__(self):
-        return len(self._hits)
+        return len(self.hits)
 
     def __getitem__(self, idx):
-        return self._hits[idx]
+        return self.hits[idx]
 
     def as_vector(self, subject_index):
         """Return the hits as a one-dimensional NumPy array of scores, using a
            subject index as the source of subjects."""
 
-        vector = np.zeros(len(subject_index))
-        for hit in self._hits:
-            subject_id = subject_index.by_uri(hit.uri)
-            vector[subject_id] = hit.score
-        return vector
+        if self._vector is None:
+            self._vector = np.zeros(len(subject_index))
+            for hit in self._hits:
+                subject_id = subject_index.by_uri(hit.uri)
+                self._vector[subject_id] = hit.score
+        return self._vector
