4a3d2fb75c88cd98989feb48f534743286d274d6
Osma Suominen <osma.suominen@helsinki.fi>
2018-11-06 13:49:33 +0200

    Make sure annif.yaml is included in installed package. Fixes #199

--- annif/__init__.py
+++ annif/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import os.path
 import logging
 import connexion
 from flask_cors import CORS
@@ -14,7 +15,8 @@ def create_app(script_info=None, config_name=None):
     # 'cxapp' here is the Connexion application that has a normal Flask app
     # as a property (cxapp.app)
 
-    cxapp = connexion.App(__name__, specification_dir='../swagger/')
+    specdir = os.path.join(os.path.dirname(__file__), 'swagger')
+    cxapp = connexion.App(__name__, specification_dir=specdir)
     if config_name is None:
         config_name = os.environ.get('ANNIF_CONFIG')
     if config_name is None:
