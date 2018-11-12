bfa5b9c6198bafa09f3717a48217d36ac02081cd
Osma Suominen <osma.suominen@helsinki.fi>
2018-11-12 13:50:51 +0200

    refactor: align pav and ensemble backends to get rid of duplicate code

--- annif/backend/pav.py
+++ annif/backend/pav.py
@@ -39,7 +39,7 @@ class PAVBackend(ensemble.EnsembleBackend):
                     project_id=project.project_id)
         return self._models[source_project_id]
 
-    def _apply_pav(self, hits, source_project, project):
+    def _normalize_hits(self, hits, source_project, project):
         reg_models = self._get_model(source_project.project_id, project)
         pav_result = []
         for hit in hits.hits:
@@ -55,30 +55,6 @@ class PAVBackend(ensemble.EnsembleBackend):
         pav_result.sort(key=lambda hit: hit.score, reverse=True)
         return annif.hit.ListAnalysisResult(
             pav_result, source_project.subjects)
-
-    def _analyze_with_sources(self, text, sources, project):
-        hits_from_sources = []
-        for project_id, weight in sources:
-            source_project = annif.project.get_project(project_id)
-            hits = source_project.analyze(text)
-            self.debug(
-                'Got {} hits from project {}'.format(
-                    len(hits), source_project.project_id))
-            pav_hits = self._apply_pav(hits, source_project, project)
-            self.debug(
-                'Got {} hits after PAV'.format(len(pav_hits)))
-            hits_from_sources.append(
-                annif.hit.WeightedHits(
-                    hits=pav_hits, weight=weight))
-        return hits_from_sources
-
-    def _analyze(self, text, project, params):
-        sources = annif.util.parse_sources(params['sources'])
-        hits_from_sources = self._analyze_with_sources(text, sources, project)
-        merged_hits = annif.util.merge_hits(
-            hits_from_sources, project.subjects)
-        self.debug('{} hits after merging'.format(len(merged_hits)))
-        return merged_hits
 
     @classmethod
     def _analyze_train_corpus(cls, source_project, corpus):
