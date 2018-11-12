60df6108864774bdaf210a2de95b1e906c70a137
Osma Suominen <osma.suominen@helsinki.fi>
2018-11-12 13:44:44 +0200

    align method signatures

--- annif/backend/ensemble.py
+++ annif/backend/ensemble.py
@@ -11,7 +11,7 @@ class EnsembleBackend(backend.AnnifBackend):
     """Ensemble backend that combines results from multiple projects"""
     name = "ensemble"
 
-    def _analyze_with_sources(self, text, sources):
+    def _analyze_with_sources(self, text, sources, project):
         hits_from_sources = []
         for project_id, weight in sources:
             project = annif.project.get_project(project_id)
@@ -26,7 +26,7 @@ class EnsembleBackend(backend.AnnifBackend):
 
     def _analyze(self, text, project, params):
         sources = annif.util.parse_sources(params['sources'])
-        hits_from_sources = self._analyze_with_sources(text, sources)
+        hits_from_sources = self._analyze_with_sources(text, sources, project)
         merged_hits = annif.util.merge_hits(
             hits_from_sources, project.subjects)
         self.debug('{} hits after merging'.format(len(merged_hits)))
