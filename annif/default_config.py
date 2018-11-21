d916481f3516b9fc6a9bf53de9c6e552239740a2
Osma Suominen <osma.suominen@helsinki.fi>
2018-11-21 11:26:03 +0200

    Make it possible to set ANNIF_DATADIR and ANNIF_PROJECTS using environment variables. Fixes #211

--- annif/default_config.py
+++ annif/default_config.py
@@ -5,12 +5,14 @@ A configuration module, where "Config" is a default configuration and the other
 classes are different configuration profiles overriding default settings.
 """
 
+import os
+
 
 class Config(object):
     DEBUG = False
     TESTING = False
-    PROJECTS_FILE = 'projects.cfg'
-    DATADIR = 'data'
+    PROJECTS_FILE = os.environ.get('ANNIF_PROJECTS', default='projects.cfg')
+    DATADIR = os.environ.get('ANNIF_DATADIR', default='data')
     INITIALIZE_PROJECTS = False
 
 
