959e5e97a3f27473cf8f3314e8e8c9b868848dd4
Osma Suominen <osma.suominen@helsinki.fi>
2018-11-21 11:48:13 +0200

    Depend on connexion[swagger-ui] which is separate since connexion 2.0. Fixes #213

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'connexion',
+        'connexion[swagger-ui]',
         'flask-cors',
         'click-log',
         'nltk',
