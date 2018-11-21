c43b25978a42c47cd83069ca354183ea222aec49
Osma Suominen <osma.suominen@helsinki.fi>
2018-11-21 13:12:56 +0200

    Bump version: 0.36.3 → 0.36.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='annif',
-    version='0.36.3',
+    version='0.36.4',
     url='https://github.com/NatLibFi/Annif',
     author='Osma Suominen',
     author_email='osma.suominen@helsinki.fi',
