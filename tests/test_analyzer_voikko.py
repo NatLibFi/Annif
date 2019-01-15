3e3bf3077a637c6e1892d96211f52358be37560e
Osma Suominen <osma.suominen@helsinki.fi>
2019-01-15 14:16:04 +0200

    expand voikko unit tests for full coverage

--- tests/test_analyzer_voikko.py
+++ tests/test_analyzer_voikko.py
@@ -6,7 +6,14 @@ import annif.analyzer
 voikko = pytest.importorskip("voikko")
 
 
+def test_voikko_getstate():
+    analyzer = annif.analyzer.get_analyzer("voikko(fi)")
+    state = analyzer.__getstate__()
+    assert state == {'param': 'fi', 'voikko': None}
+
+
 def test_voikko_finnish_analyzer_normalize_word():
     analyzer = annif.analyzer.get_analyzer("voikko(fi)")
+    assert analyzer.normalize_word("xyzzy") == "xyzzy"
     assert analyzer.normalize_word("vanhat") == "vanha"
     assert analyzer.normalize_word("koirien") == "koira"
