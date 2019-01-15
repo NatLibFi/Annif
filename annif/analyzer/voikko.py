4f77397668c4b154a9d25462d2e6565b2f6db1dc
Osma Suominen <osma.suominen@helsinki.fi>
2019-01-15 12:02:55 +0200

    Fix pickling issue with Voikko object

--- annif/analyzer/voikko.py
+++ annif/analyzer/voikko.py
@@ -10,10 +10,19 @@ class VoikkoAnalyzer(analyzer.Analyzer):
 
     def __init__(self, param):
         self.param = param
-        self.voikko = voikko.libvoikko.Voikko(param)
+        self.voikko = None
+
+    def __getstate__(self):
+        """Return the state of the object for pickling purposes. The Voikko
+        instance is set to None because as a ctypes object it cannot be
+        pickled."""
+
+        return {'param': self.param, 'voikko': None}
 
     @functools.lru_cache(maxsize=500000)
     def normalize_word(self, word):
+        if self.voikko is None:
+            self.voikko = voikko.libvoikko.Voikko(self.param)
         result = self.voikko.analyze(word)
         if len(result) > 0 and 'BASEFORM' in result[0]:
             return result[0]['BASEFORM']
