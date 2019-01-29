89d4a5210274b75d992495e786a2022e661294be
Osma Suominen <osma.suominen@helsinki.fi>
2019-01-29 15:10:08 +0200

    ChunkingBackend: handle special case when there is no text to analyze

--- annif/backend/mixins.py
+++ annif/backend/mixins.py
@@ -2,6 +2,7 @@
 
 
 import abc
+from annif.hit import ListAnalysisResult
 
 
 class ChunkingBackend(metaclass=abc.ABCMeta):
@@ -28,4 +29,6 @@ class ChunkingBackend(metaclass=abc.ABCMeta):
             if normalized != '':
                 chunktexts.append(normalized)
         self.debug('Split sentences into {} chunks'.format(len(chunktexts)))
+        if len(chunktexts) == 0:  # nothing to analyze, empty result
+            return ListAnalysisResult(hits=[], subject_index=project.subjects)
         return self._analyze_chunks(chunktexts, project)
