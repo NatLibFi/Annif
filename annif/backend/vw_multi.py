94d9ff9cd70c3016cc50493f47c87f74d40292fd
Osma Suominen <osma.suominen@helsinki.fi>
2019-01-30 13:55:10 +0200

    Remove unused import

--- annif/backend/vw_multi.py
+++ annif/backend/vw_multi.py
@@ -6,7 +6,7 @@ import os.path
 import annif.util
 from vowpalwabbit import pyvw
 import numpy as np
-from annif.hit import AnalysisHit, VectorAnalysisResult
+from annif.hit import VectorAnalysisResult
 from annif.exception import ConfigurationException, NotInitializedException
 from . import backend
 from . import mixins
