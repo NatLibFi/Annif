4877f30ac56445e8de76acfde0f75742ac21a394
Osma Suominen <osma.suominen@helsinki.fi>
2019-01-30 15:17:31 +0200

    Reorganize vw_multi tests; add test for analyze with ect algorithm

--- tests/test_backend_vw.py
+++ tests/test_backend_vw.py
@@ -60,22 +60,6 @@ def test_vw_train_multiple_passes(datadir, document_corpus, project):
     assert datadir.join('vw-model').size() > 0
 
 
-def test_vw_train_ect(datadir, document_corpus, project):
-    vw_type = annif.backend.get_backend('vw_multi')
-    vw = vw_type(
-        backend_id='vw_multi',
-        params={
-            'chunksize': 4,
-            'learning_rate': 0.5,
-            'algorithm': 'ect'},
-        datadir=str(datadir))
-
-    vw.train(document_corpus, project)
-    assert vw._model is not None
-    assert datadir.join('vw-model').exists()
-    assert datadir.join('vw-model').size() > 0
-
-
 def test_vw_train_invalid_algorithm(datadir, document_corpus, project):
     vw_type = annif.backend.get_backend('vw_multi')
     vw = vw_type(
@@ -155,3 +139,37 @@ def test_vw_analyze_empty(datadir, project):
     results = vw.analyze("...", project)
 
     assert len(results) == 0
+
+
+def test_vw_train_ect(datadir, document_corpus, project):
+    vw_type = annif.backend.get_backend('vw_multi')
+    vw = vw_type(
+        backend_id='vw_multi',
+        params={
+            'chunksize': 4,
+            'learning_rate': 0.5,
+            'algorithm': 'ect'},
+        datadir=str(datadir))
+
+    vw.train(document_corpus, project)
+    assert vw._model is not None
+    assert datadir.join('vw-model').exists()
+    assert datadir.join('vw-model').size() > 0
+
+
+def test_vw_analyze_ect(datadir, project):
+    vw_type = annif.backend.get_backend('vw_multi')
+    vw = vw_type(
+        backend_id='vw_multi',
+        params={'chunksize': 1,
+                'algorithm': 'ect'},
+        datadir=str(datadir))
+
+    results = vw.analyze("""Arkeologiaa sanotaan joskus myös
+        muinaistutkimukseksi tai muinaistieteeksi. Se on humanistinen tiede
+        tai oikeammin joukko tieteitä, jotka tutkivat ihmisen menneisyyttä.
+        Tutkimusta tehdään analysoimalla muinaisjäännöksiä eli niitä jälkiä,
+        joita ihmisten toiminta on jättänyt maaperään tai vesistöjen
+        pohjaan.""", project)
+
+    assert len(results) > 0
