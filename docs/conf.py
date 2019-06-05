81933d1b2c9f806452bbd4e5a6cdfde1874981e1
Juho Inkinen <juho.inkinen@helsinki.fi>
2019-06-05 16:23:01 +0300

    Explicitly assign master doc to try fix "contents.rst not found"

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,8 @@ version = release
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
