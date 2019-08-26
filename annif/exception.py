da7b020254f860c9d341fa70f3d84d4218bc7aef
Osma Suominen <osma.suominen@helsinki.fi>
2019-08-26 15:15:57 +0300

    Turn AnnifException into an abstract base class and fix message prefix behavior

--- annif/exception.py
+++ annif/exception.py
@@ -1,10 +1,11 @@
 """Custom exceptions used by Annif"""
 
 
+import abc
 from click import ClickException
 
 
-class AnnifException(ClickException):
+class AnnifException(ClickException, metaclass=abc.ABCMeta):
     """Base Annif exception. We define this as a subclass of ClickException so
     that the CLI can automatically handle exceptions."""
 
@@ -13,22 +14,10 @@ class AnnifException(ClickException):
         self.project_id = project_id
         self.backend_id = backend_id
 
-    def format_message(self):
-        if self.project_id is not None:
-            return "Project '{}': {}".format(self.project_id,
-                                             self.message)
-        if self.backend_id is not None:
-            return "Backend '{}': {}".format(self.backend_id,
-                                             self.message)
-        return "Error: {}".format(self.message)
-
-
-class NotInitializedException(AnnifException):
-    """Exception raised for attempting to use a project or backend that
-    cannot be initialized, most likely since it is not yet functional
-    because of lack of vocabulary or training."""
-
-    prefix = "Couldn't initialize"
+    @property
+    @abc.abstractmethod
+    def prefix(self):
+        pass
 
     def format_message(self):
         if self.project_id is not None:
@@ -42,14 +31,28 @@ class NotInitializedException(AnnifException):
         return "{}: {}".format(self.prefix, self.message)
 
 
+class NotInitializedException(AnnifException):
+    """Exception raised for attempting to use a project or backend that
+    cannot be initialized, most likely since it is not yet functional
+    because of lack of vocabulary or training."""
+
+    @property
+    def prefix(self):
+        return "Couldn't initialize"
+
+
 class ConfigurationException(AnnifException):
     """Exception raised when a project or backend is misconfigured."""
 
-    prefix = "Misconfigured"
+    @property
+    def prefix(self):
+        return "Misconfigured"
 
 
 class NotSupportedException(AnnifException):
     """Exception raised when an operation is not supported by a project or
     backend."""
 
-    prefix = "Not supported"
+    @property
+    def prefix(self):
+        return "Not supported"
