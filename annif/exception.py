0ecacf4130822e59e4d52aa31f8a368d1730e78f
Osma Suominen <osma.suominen@helsinki.fi>
2019-08-27 09:40:09 +0300

    Workaround for making AnnifException abstract (non-instantiable), since Exception subclasses cannot be made abstract using the abc module.

--- annif/exception.py
+++ annif/exception.py
@@ -1,23 +1,24 @@
 """Custom exceptions used by Annif"""
 
 
-import abc
 from click import ClickException
 
 
-class AnnifException(ClickException, metaclass=abc.ABCMeta):
+class AnnifException(ClickException):
     """Base Annif exception. We define this as a subclass of ClickException so
-    that the CLI can automatically handle exceptions."""
+    that the CLI can automatically handle exceptions. This exception cannot be
+    instantiated directly - subclasses should be used instead."""
 
     def __init__(self, message, project_id=None, backend_id=None):
         super().__init__(message)
         self.project_id = project_id
         self.backend_id = backend_id
 
-    @property
-    @abc.abstractmethod
-    def prefix(self):
-        pass
+        if self.prefix is None:
+            raise TypeError("Cannot instantiate exception without a prefix.")
+
+    # subclasses should set this to a descriptive prefix
+    prefix = None
 
     def format_message(self):
         if self.project_id is not None:
@@ -36,23 +37,17 @@ class NotInitializedException(AnnifException):
     cannot be initialized, most likely since it is not yet functional
     because of lack of vocabulary or training."""
 
-    @property
-    def prefix(self):
-        return "Couldn't initialize"
+    prefix = "Couldn't initialize"
 
 
 class ConfigurationException(AnnifException):
     """Exception raised when a project or backend is misconfigured."""
 
-    @property
-    def prefix(self):
-        return "Misconfigured"
+    prefix = "Misconfigured"
 
 
 class NotSupportedException(AnnifException):
     """Exception raised when an operation is not supported by a project or
     backend."""
 
-    @property
-    def prefix(self):
-        return "Not supported"
+    prefix = "Not supported"
