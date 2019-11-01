99dd6a88c234ad00ef4af0b6d55e4af6d8250348
Juho Inkinen <juho.inkinen@helsinki.fi>
2019-11-01 09:48:27 +0200

    Revert "Convert hit scores to (default) float"
    This reverts commit 0c936e4450e4bb88208e300ebc0d34db31c2cea5.

--- annif/rest.py
+++ annif/rest.py
@@ -64,8 +64,7 @@ def suggest(project_id, text, limit, threshold):
     except AnnifException as err:
         return server_error(err)
     hits = hit_filter(result)
-    return {'results':
-        [hit._replace(score=float(hit.score))._asdict() for hit in hits]}
+    return {'results': [hit._asdict() for hit in hits]}
 
 
 def _documents_to_corpus(documents):
