b1d8f3ea8d6bf5d0348a3c9c1a2a42bc4ec65a65
juhoinkinen <juho.inkinen@gmail.com>
2019-12-16 22:13:31 +0200

    NN-ensemble backend-param test

--- tests/test_backend_nn_ensemble.py
+++ tests/test_backend_nn_ensemble.py
@@ -56,6 +56,32 @@ def test_nn_ensemble_train_and_learn(app, tmpdir):
     assert modelfile.size() != old_size or modelfile.mtime() != old_mtime
 
 
+def test_nn_ensemble_train_and_learn_params(app, tmpdir, capfd):
+    project = annif.project.get_project('dummy-en')
+    nn_ensemble_type = annif.backend.get_backend("nn_ensemble")
+    nn_ensemble = nn_ensemble_type(
+        backend_id='nn_ensemble',
+        config_params={'sources': 'dummy-en'},
+        project=project)
+
+    tmpfile = tmpdir.join('document.tsv')
+    tmpfile.write("dummy\thttp://example.org/dummy\n" +
+                  "another\thttp://example.org/dummy\n" +
+                  "none\thttp://example.org/none")
+    document_corpus = annif.corpus.DocumentFile(str(tmpfile))
+
+    train_params = {'nn_ensemble': {'epochs': 3}}
+    with app.app_context():
+        nn_ensemble.train(document_corpus, train_params)
+    out, _ = capfd.readouterr()
+    assert 'Epoch 3/3' in out
+
+    learn_params = {'nn_ensemble': {'learn-epochs': 2}}
+    nn_ensemble.learn(document_corpus, learn_params)
+    out, _ = capfd.readouterr()
+    assert 'Epoch 2/2' in out
+
+
 def test_nn_ensemble_initialize(app, app_project):
     nn_ensemble_type = annif.backend.get_backend("nn_ensemble")
     nn_ensemble = nn_ensemble_type(
