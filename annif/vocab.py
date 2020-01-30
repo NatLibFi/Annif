3ccd9520a3ba0c73cc05358a4b8ea6fac0a1edc8
Juho Inkinen <juho.inkinen@helsinki.fi>
2020-01-30 16:29:34 +0200

    Initial method for updating subject index

--- annif/vocab.py
+++ annif/vocab.py
@@ -25,6 +25,30 @@ class AnnifVocabulary(DatadirMixin):
         self._subjects = annif.corpus.SubjectIndex(subject_corpus)
         annif.util.atomic_save(self._subjects, self.datadir, 'subjects')
 
+    def _update_subject_index(self, subject_corpus):
+        old_subjects = self.subjects
+        new_subjects = annif.corpus.SubjectIndex(subject_corpus)
+        updated_subjects = annif.corpus.SubjectIndex.load(os.path.devnull)
+
+        for subject_id in range(len(old_subjects)):
+            old_uri = old_subjects[subject_id][0]
+            new_ind = new_subjects.by_uri(old_uri)
+            if new_ind is None:  # subject removed from new corpus
+                new_label = ''
+            else:
+                new_label = new_subjects[new_ind][1]
+            updated_subjects.append(old_uri, new_label)
+
+        for subject_id in range(len(new_subjects)):
+            new_uri = new_subjects[subject_id][0]
+            new_label = new_subjects[subject_id][1]
+            if new_uri in old_subjects._uris:
+                continue
+            updated_subjects.append(new_uri, new_label)
+
+        self._subjects = updated_subjects
+        annif.util.atomic_save(self._subjects, self.datadir, 'subjects')
+
     @property
     def subjects(self):
         if self._subjects is None:
@@ -41,7 +65,11 @@ class AnnifVocabulary(DatadirMixin):
         """load subjects from a subject corpus and save them into a
         SKOS/Turtle file for later use"""
 
-        self._create_subject_index(subject_corpus)
+        if os.path.exists(os.path.join(self.datadir, 'subjects')):
+            logger.info('updating existing vocabulary')
+            self._update_subject_index(subject_corpus)
+        else:
+            self._create_subject_index(subject_corpus)
         subject_corpus.save_skos(os.path.join(self.datadir, 'subjects.ttl'),
                                  language)
 
