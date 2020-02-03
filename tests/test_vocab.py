a24613b1c53e21e395d391c0f47ff6ec58ad31e8
Juho Inkinen <juho.inkinen@helsinki.fi>
2020-02-03 15:43:07 +0200

    Split test

--- tests/test_vocab.py
+++ tests/test_vocab.py
@@ -5,46 +5,78 @@ import annif.corpus
 import annif.vocab
 
 
-def test_update_subject_index(testdatadir, tmpdir, subject_index):
+def load_dummy_vocab(tmpdir):
+    vocab = annif.vocab.AnnifVocabulary('vocab-id', str(tmpdir))
     subjfile = os.path.join(
         os.path.dirname(__file__),
         'corpora',
         'dummy-subjects.tsv')
-    vocab = annif.vocab.AnnifVocabulary('vocab-id', str(tmpdir))
+    subjects = annif.corpus.SubjectFileTSV(subjfile)
+    vocab.load_vocabulary(subjects, 'en')
+    return vocab
+
+
+def test_update_subject_index_with_no_changes(tmpdir):
+    vocab = load_dummy_vocab(tmpdir)
+
+    subjfile = os.path.join(
+        os.path.dirname(__file__),
+        'corpora',
+        'dummy-subjects.tsv')
     subjects = annif.corpus.SubjectFileTSV(subjfile)
 
-    # Load vocabulary first time
-    vocab.load_vocabulary(subjects, 'en')
-
-    assert len(vocab.subjects) == 2
-    assert vocab.subjects.by_uri('http://example.org/dummy') == 0
-    assert vocab.subjects[0] == ('http://example.org/dummy', 'dummy')
-
-    # Load the same vocabulary again, nothing changes
     vocab.load_vocabulary(subjects, 'en')
     assert len(vocab.subjects) == 2
     assert vocab.subjects.by_uri('http://example.org/dummy') == 0
     assert vocab.subjects[0] == ('http://example.org/dummy', 'dummy')
+    assert vocab.subjects.by_uri('http://example.org/none') == 1
+    assert vocab.subjects[1] == ('http://example.org/none', 'none')
 
-    # Update with new vocabulary
+
+def test_update_subject_index_with_removed_subject(tmpdir):
+    vocab = load_dummy_vocab(tmpdir)
+
     subjfile_new = tmpdir.join('subjects_new.tsv')
-    subjfile_new.write("<http://example.org/dummy>\tdummy\n" +
-                       "<http://example.org/dummydummy>\tdummydummy\n" +
-                       "<http://example.org/nonenone>\tnonenone\n")
+    subjfile_new.write("<http://example.org/dummy>\tdummy\n")
     subjects_new = annif.corpus.SubjectFileTSV(str(subjfile_new))
+
     vocab.load_vocabulary(subjects_new, 'en')
-    assert len(vocab.subjects) == 4
+    assert len(vocab.subjects) == 2
     assert vocab.subjects.by_uri('http://example.org/dummy') == 0
     assert vocab.subjects[0] == ('http://example.org/dummy', 'dummy')
     assert vocab.subjects.by_uri('http://example.org/none') == 1
     assert vocab.subjects[1] == ('http://example.org/none', '')
-    assert vocab.subjects.by_uri('http://example.org/dummydummy') == 2
-    assert vocab.subjects[2] == ('http://example.org/dummydummy', 'dummydummy')
 
-    # Update by loading the original back
-    vocab.load_vocabulary(subjects, 'en')
+
+def test_update_subject_index_with_renamed_label(tmpdir):
+    vocab = load_dummy_vocab(tmpdir)
+
+    subjfile_new = tmpdir.join('subjects_new.tsv')
+    subjfile_new.write("<http://example.org/dummy>\tdummy\n" +
+                       "<http://example.org/none>\tnew none\n")
+    subjects_new = annif.corpus.SubjectFileTSV(str(subjfile_new))
+
+    vocab.load_vocabulary(subjects_new, 'en')
+    assert len(vocab.subjects) == 2
+    assert vocab.subjects.by_uri('http://example.org/dummy') == 0
+    assert vocab.subjects[0] == ('http://example.org/dummy', 'dummy')
+    assert vocab.subjects.by_uri('http://example.org/none') == 1
+    assert vocab.subjects[1] == ('http://example.org/none', 'new none')
+
+
+def test_update_subject_index_with_added_subjects(tmpdir):
+    vocab = load_dummy_vocab(tmpdir)
+
+    subjfile_new = tmpdir.join('subjects_new.tsv')
+    subjfile_new.write("<http://example.org/dummy>\tdummy\n" +
+                       "<http://example.org/none>\tnone\n" +
+                       "<http://example.org/new-dummy>\tnew dummy\n" +
+                       "<http://example.org/new-none>\tnew none\n")
+    subjects_new = annif.corpus.SubjectFileTSV(str(subjfile_new))
+
+    vocab.load_vocabulary(subjects_new, 'en')
     assert len(vocab.subjects) == 4
     assert vocab.subjects.by_uri('http://example.org/dummy') == 0
     assert vocab.subjects[0] == ('http://example.org/dummy', 'dummy')
-    assert vocab.subjects.by_uri('http://example.org/dummydummy') == 2
-    assert vocab.subjects[2] == ('http://example.org/dummydummy', '')
+    assert vocab.subjects.by_uri('http://example.org/new-dummy') == 2
+    assert vocab.subjects[2] == ('http://example.org/new-dummy', 'new dummy')
