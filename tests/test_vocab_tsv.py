461f57da49067afb758ee3e33a660899e32ea39d
Juho Inkinen <juho.inkinen@helsinki.fi>
2020-02-05 17:21:53 +0200

    Test for loading vocabulary with notations

--- tests/test_vocab_tsv.py
+++ tests/test_vocab_tsv.py
@@ -28,3 +28,16 @@ def test_load_tsv_uri_nobrackets(tmpdir):
     assert index[1] == (
         'http://www.yso.fi/onto/yso/p9285',
         'neoliittinen kausi', None)
+
+
+def test_load_tsv_with_notations(tmpdir):
+    tmpfile = tmpdir.join('subjects-with-notations.tsv')
+    tmpfile.write("http://www.yso.fi/onto/yso/p8993\thylyt\t42.42\n" +
+                  "http://www.yso.fi/onto/yso/p9285\tneoliittinen kausi\t42.0")
+
+    index = SubjectIndex.load(str(tmpfile))
+    assert len(index) == 2
+    assert index[0] == ('http://www.yso.fi/onto/yso/p8993', 'hylyt', '42.42')
+    assert index[1] == (
+        'http://www.yso.fi/onto/yso/p9285',
+        'neoliittinen kausi', '42.0')
