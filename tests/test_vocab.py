6e8324d2286c7c62320726c12ad54bd9412d3835
Juho Inkinen <juho.inkinen@helsinki.fi>
2020-02-13 17:33:32 +0200

    Test updating also notation when updating vocab

--- tests/test_vocab.py
+++ tests/test_vocab.py
@@ -48,12 +48,12 @@ def test_update_subject_index_with_removed_subject(tmpdir):
     assert vocab.subjects[1] == ('http://example.org/none', '', None)
 
 
-def test_update_subject_index_with_renamed_label(tmpdir):
+def test_update_subject_index_with_renamed_label_and_added_notation(tmpdir):
     vocab = load_dummy_vocab(tmpdir)
 
     subjfile_new = tmpdir.join('subjects_new.tsv')
     subjfile_new.write("<http://example.org/dummy>\tdummy\n" +
-                       "<http://example.org/none>\tnew none\n")
+                       "<http://example.org/none>\tnew none\t42.42\n")
     subjects_new = annif.corpus.SubjectFileTSV(str(subjfile_new))
 
     vocab.load_vocabulary(subjects_new, 'en')
@@ -61,16 +61,16 @@ def test_update_subject_index_with_renamed_label(tmpdir):
     assert vocab.subjects.by_uri('http://example.org/dummy') == 0
     assert vocab.subjects[0] == ('http://example.org/dummy', 'dummy', None)
     assert vocab.subjects.by_uri('http://example.org/none') == 1
-    assert vocab.subjects[1] == ('http://example.org/none', 'new none', None)
+    assert vocab.subjects[1] == ('http://example.org/none', 'new none',
+                                 '42.42')
 
 
 def test_update_subject_index_with_added_subjects(tmpdir):
     vocab = load_dummy_vocab(tmpdir)
-
     subjfile_new = tmpdir.join('subjects_new.tsv')
     subjfile_new.write("<http://example.org/dummy>\tdummy\n" +
                        "<http://example.org/none>\tnone\n" +
-                       "<http://example.org/new-dummy>\tnew dummy\n" +
+                       "<http://example.org/new-dummy>\tnew dummy\t42.42\n" +
                        "<http://example.org/new-none>\tnew none\n")
     subjects_new = annif.corpus.SubjectFileTSV(str(subjfile_new))
 
@@ -80,4 +80,4 @@ def test_update_subject_index_with_added_subjects(tmpdir):
     assert vocab.subjects[0] == ('http://example.org/dummy', 'dummy', None)
     assert vocab.subjects.by_uri('http://example.org/new-dummy') == 2
     assert vocab.subjects[2] == ('http://example.org/new-dummy', 'new dummy',
-                                 None)
+                                 '42.42')
