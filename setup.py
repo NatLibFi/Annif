79c52e39c5f6c0b81850714abb19d78e714394d5
Juho Inkinen <juho.inkinen@helsinki.fi>
2020-05-12 17:11:05 +0300

    Manual version bump to 0.47.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='annif',
-    version='0.47.0',
+    version='0.47.1',
     url='https://github.com/NatLibFi/Annif',
     author='Osma Suominen',
     author_email='osma.suominen@helsinki.fi',
