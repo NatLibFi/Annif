b408a2204bc3c887c59e83aedbaddfd4aa5861e1
Osma Suominen <osma.suominen@helsinki.fi>
2020-05-13 18:20:16 +0300

    Pin fasttext version to 0.9.2 and fix fasttext version in Dockerfile

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     ],
     tests_require=['py', 'pytest', 'requests'],
     extras_require={
-        'fasttext': ['fasttext'],
+        'fasttext': ['fasttext==0.9.2'],
         'voikko': ['voikko'],
         'vw': ['vowpalwabbit==8.7.*'],
         'nn': ['tensorflow-cpu==2.2.0', 'lmdb==0.98'],
