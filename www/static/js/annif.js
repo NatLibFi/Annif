a86e8062e80f0a68e0fee83dc69a38b9beec4ab3
Osma Suominen <osma.suominen@helsinki.fi>
2020-06-17 10:39:07 +0300

    Rework the form section, adapting work from Finto AI

--- www/static/js/annif.js
+++ www/static/js/annif.js
@@ -3,9 +3,10 @@ var base_url = 'https://api.annif.org/v1/';
 
 function clearResults() {
     $('#results').empty();
+    $('#suggestions').hide();
 }
 
-function fetch_projects() {
+function fetchProjects() {
     $.ajax({
         url: base_url + "projects",
         method: 'GET',
@@ -20,32 +21,67 @@ function fetch_projects() {
     });
 }
 
-function analyze() {
+function getSuggestions() {
+    $('#suggestions').show();
+    $('#results-spinner').show();
     $.ajax({
         url: base_url + "projects/" + $('#project').val() + "/suggest",
         method: 'POST',
-        data: { 
+        data: {
           text: $('#text').val(),
+          limit: $('input[name="limit"]:checked').val(),
+          threshold: 0.001
         },
         success: function(data) {
-            var firstscore = null;
+            $('#results-spinner').hide();
+
+            if (data.results.length == 0) {
+                $('#results').hide();
+                $('#no-results').show();
+            }
             $.each(data.results, function(idx, value) {
-                if (firstscore == null) { firstscore = value.score }
+                $('#no-results').hide();
                 $('#results').append(
                     $('<li class="list-group-item p-0">').append(
-                        $('<meter class="mr-2">').attr('value',value.score).attr('max',firstscore),
-                        $('<a>').attr('href',value.uri).append(value.label)
+                        $('<meter class="mr-2">').attr('value',value.score).attr('max',1.0).attr('title',value.score.toFixed(4)),
+                        $('<a target="_blank">').attr('href',value.uri).append(value.label)
                     )
                 );
+                $('#results').show();
             });
         }
     });
 }
 
+function disableButton() {
+    $('#get-suggestions').prop("disabled", true);
+}
+
+function enableButton() {
+    $('#get-suggestions').prop("disabled", false);
+}
+
 $(document).ready(function() {
-    fetch_projects();
-    $('#analyze').click(function() {
+    $('#no-results').hide();
+    $('#results-spinner').hide();
+    clearResults();
+    if ($.trim($('#text').val()) != "") {
+        enableButton();
+    } else {
+        disableButton();
+    }
+    fetchProjects();
+    $('#get-suggestions').click(function() {
         clearResults();
-        analyze();
+        getSuggestions();
+    });
+    $('#button-clear').click(function() {
+        $('#text').val('');
+        $('#text').focus();
+        clearResults();
+        disableButton();
+    });
+    $('#text').on("input", function() {
+        enableButton();
     });
 });
