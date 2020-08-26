b6a49b3cba3c4fa919b1f70a77166636a9964b9f
juhoinkinen <juho.inkinen@gmail.com>
2020-08-26 12:40:08 +0300

    Add menu for selecting labels language

--- ai.finto.fi/static/js/annif.js
+++ ai.finto.fi/static/js/annif.js
@@ -27,6 +27,38 @@ function fetchProjects() {
     });
 }
 
+function makeLabelLanguageOptions() {
+    $('#label-language').append(
+        $('<option>').attr('value','project-language').attr('data-i18n','label-language-option-project'),
+        $('<option>').attr('value','fi').attr('data-i18n','label-language-option-fi'),
+        $('<option>').attr('value','sv').attr('data-i18n','label-language-option-sv'),
+        $('<option>').attr('value','en').attr('data-i18n','label-language-option-en'),
+    );
+}
+
+function getLabelPromise(uri, lang) {
+    return $.ajax({
+        // TODO: Define base url for api.finto somewhere else?
+        // TODO: How to handle URI to YSO-places, which is not found from YSO?
+        // TODO: Also modify the URI to point to the language version of YSO corresponding to UI language (via data-i18n)?
+        url: "http://api.finto.fi/rest/v1/yso/label?uri=" + uri + "&lang=" + lang,
+        method: 'GET'
+    });
+}
+
+function showResults(data) {
+    $.each(data.results, function(idx, value) {
+        $('#no-results').hide();
+        $('#results').append(
+            $('<li class="list-group-item p-0">').append(
+                $('<meter class="mr-2">').attr('value',value.score).attr('max',1.0).attr('title',value.score.toFixed(4)),
+                $('<a target="_blank">').attr('href',value.uri).append(value.label)
+            )
+        );
+        $('#results').show();
+    });
+}
+
 function getSuggestions() {
     $('#suggestions').show();
     $('#results-spinner').show();
@@ -45,16 +77,26 @@ function getSuggestions() {
                 $('#results').hide();
                 $('#no-results').show();
             }
-            $.each(data.results, function(idx, value) {
-                $('#no-results').hide();
-                $('#results').append(
-                    $('<li class="list-group-item p-0">').append(
-                        $('<meter class="mr-2">').attr('value',value.score).attr('max',1.0).attr('title',value.score.toFixed(4)),
-                        $('<a target="_blank">').attr('href',value.uri).append(value.label)
-                    )
-                );
-                $('#results').show();
-            });
+
+            if ($('#label-language').val() == 'project-language') {
+                showResults(data);
+            }
+            else {
+                var promises = []
+                $.each(data.results, function(idx, value) {
+                    promises.push(
+                        getLabelPromise(value.uri, $('#label-language').val())
+                    );
+                });
+                $.when.apply($, promises).done(function(result) {
+                    $.each(promises, function(idx, promise) {
+                        // TODO: Make sure label is taken from the right URI (promises and results are in the same order)
+                        // TODO: What to do if api.finto does not respond?
+                        data.results[idx].label = promise.responseJSON.prefLabel;
+                    });
+                    showResults(data);
+                });
+            }
         }
     });
 }
@@ -77,6 +119,7 @@ $(document).ready(function() {
         disableButton();
     }
     fetchProjects();
+    makeLabelLanguageOptions();
     $('#get-suggestions').click(function() {
         clearResults();
         getSuggestions();
