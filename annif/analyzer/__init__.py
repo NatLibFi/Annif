2d47147e2a88f9089e5c20674f8abbc67fc7810a
Moritz Fuerneisen <m.fuerneisen@zbw.eu>
2021-03-24 09:48:47 +0100

    Reduce complexity in analyzer init method.

--- annif/analyzer/__init__.py
+++ annif/analyzer/__init__.py
@@ -12,6 +12,27 @@ def register_analyzer(analyzer):
     _analyzers[analyzer.name] = analyzer
 
 
+def _extend_posargs(posargs):
+    if not posargs:
+        posargs = [None]
+    return posargs
+
+
+def _parse_analyzer_args(param_string):
+    if not param_string:
+        return [None], {}
+    kwargs = {}
+    posargs = []
+    param_strings = param_string.split(',')
+    for p_string in param_strings:
+        parts = p_string.split('=')
+        if len(parts) == 1:
+            posargs.append(p_string)
+        elif len(parts) == 2:
+            kwargs[parts[0]] = parts[1]
+    return _extend_posargs(posargs), kwargs
+
+
 def get_analyzer(analyzerspec):
     match = re.match(r'(\w+)(\((.*)\))?', analyzerspec)
     if match is None:
@@ -19,21 +40,9 @@ def get_analyzer(analyzerspec):
             "Invalid analyzer specification {}".format(analyzerspec))
 
     analyzer = match.group(1)
-    param_string = match.group(3)
-    kwargs = {}
-    pos_args = []
-    if param_string:
-        param_strings = param_string.split(',')
-        for p_string in param_strings:
-            parts = p_string.split('=')
-            if len(parts) == 1:
-                pos_args.append(p_string)
-            elif len(parts) == 2:
-                kwargs[parts[0]] = parts[1]
-    if not pos_args:
-        pos_args = [None]
+    posargs, kwargs = _parse_analyzer_args(match.group(3))
     try:
-        return _analyzers[analyzer](*pos_args, **kwargs)
+        return _analyzers[analyzer](*posargs, **kwargs)
     except KeyError:
         raise ValueError("No such analyzer {}".format(analyzer))
 
