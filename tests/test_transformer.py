9240ecd5929368aefb3ac08911630efbf16bbb7a
Juho Inkinen <juho.inkinen@helsinki.fi>
2021-06-29 09:56:02 +0300

    Test for parse_specs

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -3,6 +3,12 @@
 import pytest
 import annif.transformer
 from annif.exception import ConfigurationException
+from annif.transformer import parse_specs
+
+
+def test_parse_specs():
+    parsed = parse_specs('foo, bar(42,43,key=abc)')
+    assert parsed == [('foo', [], {}), ('bar', ['42', '43'], {'key': 'abc'})]
 
 
 def test_get_transform_nonexistent():
@@ -22,5 +28,5 @@ def test_input_limiter():
 
 def test_chained_transforms_text():
     transf = annif.transformer.get_transform(
-        "limit(5),   pass,limit(3),", project=None)
+        "limit(5),pass,limit(3),", project=None)
     assert transf.transform_text("abcdefghij") == "abc"
