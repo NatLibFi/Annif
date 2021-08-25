81f47c1722b0d6a517010493e0e012ea60fb37bc
Juho Inkinen <juho.inkinen@helsinki.fi>
2021-08-25 17:15:14 +0300

    Remove internal input-limiting from LangFilter

--- annif/transform/langfilter.py
+++ annif/transform/langfilter.py
@@ -11,16 +11,12 @@ class LangFilter(transform.BaseTransform):
 
     name = 'filter_lang'
 
-    def __init__(self, project, threshold=0.99, input_limit=None):
+    def __init__(self, project, threshold=0.99):
         super().__init__(project)
         self.threshold = float(threshold)
-        # TODO: Is input limit is useful also here for performance?
-        self.input_limit = int(input_limit) if input_limit is not None \
-            else None
 
     def transform_fn(self, text):
         retained_sentences = []
-        char_cnt = 0
         sentences = self.project.analyzer.tokenize_sentences(text)
         logger.debug(f'Number of input chars {len(text)} ' +
                      f'in {len(sentences)} sentences')
@@ -31,9 +27,6 @@ class LangFilter(transform.BaseTransform):
             if detected_lang == self.project.language \
                     and probability >= self.threshold:
                 retained_sentences.append(sent)
-                char_cnt += len(sent)
-            if self.input_limit is not None and char_cnt >= self.input_limit:
-                break
         text_out = ' '.join(retained_sentences)
         logger.debug(f'Number of retained chars {len(text_out)} ' +
                      f'in {len(retained_sentences)} sentences')
