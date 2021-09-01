bbe155bc8e55a6d844c0d3a49bdc5c7815deb7de
Juho Inkinen <juho.inkinen@helsinki.fi>
2021-09-01 14:57:04 +0300

    Add parameter text_min_length to bypass lang-filtering of short texts

--- annif/transform/langfilter.py
+++ annif/transform/langfilter.py
@@ -11,11 +11,15 @@ class LangFilter(transform.BaseTransform):
 
     name = 'filter_lang'
 
-    def __init__(self, project, sentence_min_length=50):
+    def __init__(self, project, text_min_length=500, sentence_min_length=50):
         super().__init__(project)
+        self.text_min_length = int(text_min_length)
         self.sentence_min_length = int(sentence_min_length)
 
     def transform_fn(self, text):
+        if len(text) < self.text_min_length:
+            return text
+
         retained_sentences = []
         sentences = self.project.analyzer.tokenize_sentences(text)
         logger.debug(f'Number of input chars {len(text)} ' +
