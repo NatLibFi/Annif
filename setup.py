4736426150db4479e32a5808131407e2259582e6
Juho Inkinen <juho.inkinen@helsinki.fi>
2022-01-10 18:32:37 +0200

    Upgrade scikit-learn to 1.0.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'joblib==1.1.0',
         'nltk',
         'gensim==4.1.*',
-        'scikit-learn==0.24.2',
+        'scikit-learn==1.0.2',
         'scipy==1.7.*',
         'rdflib>=4.2,<7.0',
         'gunicorn',
