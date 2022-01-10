a4af9d3110456bd9404ec84475dfb98b17f74f49
Juho Inkinen <juho.inkinen@helsinki.fi>
2022-01-10 12:15:56 +0200

    Update requirement in setup.py for dropping Python 3.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
         'connexion[swagger-ui]',
         'swagger_ui_bundle',
