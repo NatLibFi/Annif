73932e4403f54cf88a7d045c8362334fc43d9f18
Juho Inkinen <juho.inkinen@helsinki.fi>
2022-01-12 11:24:27 +0200

    Add tests for unsuccessful swagger requests

--- tests/test_swagger.py
+++ tests/test_swagger.py
@@ -22,6 +22,11 @@ def test_swagger_show_project():
     assert req.json()['project_id'] == 'dummy-fi'
 
 
+def test_swagger_show_project_nonexistent():
+    req = requests.get('http://localhost:8000/v1/projects/nonexistent')
+    assert req.status_code == 404
+
+
 def test_swagger_suggest():
     data = {'text': 'example text'}
     req = requests.post('http://localhost:8000/v1/projects/dummy-fi/suggest',
@@ -30,9 +35,37 @@ def test_swagger_suggest():
     assert 'results' in req.json()
 
 
+def test_swagger_suggest_nonexistent():
+    data = {'text': 'example text'}
+    req = requests.post(
+        'http://localhost:8000/v1/projects/nonexistent/suggest', data=data)
+    assert req.status_code == 404
+
+
+def test_swagger_suggest_novocab():
+    data = {'text': 'example text'}
+    req = requests.post('http://localhost:8000/v1/projects/novocab/suggest',
+                        data=data)
+    assert req.status_code == 503
+
+
 def test_swagger_learn():
     data = [{'text': 'the quick brown fox',
             'subjects': [{'uri': 'http://example.org/fox', 'label': 'fox'}]}]
     req = requests.post('http://localhost:8000/v1/projects/dummy-fi/learn',
                         json=data)
     assert req.status_code == 204
+
+
+def test_swagger_learn_nonexistent():
+    data = []
+    req = requests.post('http://localhost:8000/v1/projects/nonexistent/learn',
+                        json=data)
+    assert req.status_code == 404
+
+
+def test_swagger_learn_novocab():
+    data = []
+    req = requests.post('http://localhost:8000/v1/projects/novocab/learn',
+                        json=data)
+    assert req.status_code == 503
