b122ee571891c460d4de48a8cb29ecf5b771e906
Osma Suominen <osma.suominen@helsinki.fi>
2022-01-18 16:44:38 +0200

    Add option for lowercasing lemmas in spaCy analyzer

--- annif/analyzer/spacy.py
+++ annif/analyzer/spacy.py
@@ -2,6 +2,9 @@
 
 import spacy
 from . import analyzer
+import annif.util
+
+_KEY_LOWERCASE = 'lowercase'
 
 
 class SpacyAnalyzer(analyzer.Analyzer):
@@ -10,12 +13,24 @@ class SpacyAnalyzer(analyzer.Analyzer):
     def __init__(self, param, **kwargs):
         self.param = param
         self.nlp = spacy.load(param, exclude=['ner', 'parser'])
+        if _KEY_LOWERCASE in kwargs:
+            self.lowercase = annif.util.boolean(kwargs[_KEY_LOWERCASE])
+        else:
+            self.lowercase = False
         super().__init__(**kwargs)
 
     def tokenize_words(self, text):
-        return [lemma for lemma in (token.lemma_ for token in self.nlp(text))
-                if self.is_valid_token(lemma)]
+        lemmas = [lemma for lemma in (token.lemma_ for token in self.nlp(text))
+                  if self.is_valid_token(lemma)]
+        if self.lowercase:
+            return [lemma.lower() for lemma in lemmas]
+        else:
+            return lemmas
 
     def normalize_word(self, word):
         doc = self.nlp(word)
-        return doc[:].lemma_
+        lemma = doc[:].lemma_
+        if self.lowercase:
+            return lemma.lower()
+        else:
+            return lemma
--- tests/test_analyzer_spacy.py
+++ tests/test_analyzer_spacy.py
@@ -9,10 +9,19 @@ spacy = pytest.importorskip("annif.analyzer.spacy")
 def test_spacy_english_tokenize_words():
     analyzer = annif.analyzer.get_analyzer("spacy(en_core_web_sm)")
     words = analyzer.tokenize_words("""
-        The quick brown foxes jumped over the lazy dogs.
+        The quick brown foxes jumped over the lazy dogs in Paris.
         """)
     assert words == ['the', 'quick', 'brown', 'fox',
-                     'jump', 'over', 'the', 'lazy', 'dog']
+                     'jump', 'over', 'the', 'lazy', 'dog', 'Paris']
+
+
+def test_spacy_english_tokenize_words_lowercase():
+    analyzer = annif.analyzer.get_analyzer("spacy(en_core_web_sm,lowercase=1)")
+    words = analyzer.tokenize_words("""
+        The quick brown foxes jumped over the lazy dogs in Paris.
+        """)
+    assert words == ['the', 'quick', 'brown', 'fox',
+                     'jump', 'over', 'the', 'lazy', 'dog', 'paris']
 
 
 def test_spacy_english_normalize_word():
@@ -20,3 +29,9 @@ def test_spacy_english_normalize_word():
     assert analyzer.normalize_word("xyzzy") == "xyzzy"
     assert analyzer.normalize_word("older") == "old"
     assert analyzer.normalize_word("dogs") == "dog"
+    assert analyzer.normalize_word("Paris") == "Paris"
+
+
+def test_spacy_english_normalize_word_lowercase():
+    analyzer = annif.analyzer.get_analyzer("spacy(en_core_web_sm,lowercase=1)")
+    assert analyzer.normalize_word("Paris") == "paris"
