c8f94a70bcd9c13a16f3c9b4f51c96d1b3ed9f59
Osma Suominen <osma.suominen@helsinki.fi>
2022-05-27 14:28:21 +0300

    optimization: avoid pickling simplemma langdata (use load_data instead)

--- annif/analyzer/simplemma.py
+++ annif/analyzer/simplemma.py
@@ -10,9 +10,17 @@ class SimplemmaAnalyzer(analyzer.Analyzer):
 
     def __init__(self, param, **kwargs):
         self.lang = param
-        self.langdata = simplemma.load_data(self.lang)
+        self.langdata = None
         super().__init__(**kwargs)
+
+    def __getstate__(self):
+        """Return the state of the object for pickling purposes. The langdata
+        field is set to None as it's more efficient to use load_data."""
+
+        return {'lang': self.lang, 'langdata': None}
 
     @functools.lru_cache(maxsize=500000)
     def _normalize_word(self, word):
+        if self.langdata is None:
+            self.langdata = simplemma.load_data(self.lang)
         return simplemma.lemmatize(word, self.langdata)
