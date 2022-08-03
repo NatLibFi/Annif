711f7a7ac7a815807939b2a6d61fccabf857c4d5
Osma Suominen <osma.suominen@helsinki.fi>
2022-08-03 09:23:42 +0300

    adjust MLLM to not rely on labels from vocabulary, which may now be qnames

--- annif/lexical/mllm.py
+++ annif/lexical/mllm.py
@@ -146,18 +146,24 @@ class MLLMModel:
         return candidates_to_features(candidates, self._model_data)
 
     def _prepare_terms(self, graph, vocab, params):
+        pref_label_props = [SKOS.prefLabel]
         if annif.util.boolean(params['use_hidden_labels']):
-            label_props = [SKOS.altLabel, SKOS.hiddenLabel]
+            nonpref_label_props = [SKOS.altLabel, SKOS.hiddenLabel]
         else:
-            label_props = [SKOS.altLabel]
+            nonpref_label_props = [SKOS.altLabel]
 
         terms = []
         subject_ids = []
-        for subj_id, uri, pref, _ in vocab.subjects.active:
+        for subj_id, uri, _, _ in vocab.subjects.active:
             subject_ids.append(subj_id)
-            terms.append(Term(subject_id=subj_id, label=pref, is_pref=True))
 
-            for label in get_subject_labels(graph, uri, label_props,
+            for label in get_subject_labels(graph, uri, pref_label_props,
+                                            params['language']):
+                terms.append(Term(subject_id=subj_id,
+                                  label=label,
+                                  is_pref=True))
+
+            for label in get_subject_labels(graph, uri, nonpref_label_props,
                                             params['language']):
                 terms.append(Term(subject_id=subj_id,
                                   label=label,
