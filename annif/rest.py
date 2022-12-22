fb1e5795220cc866be92f950d20f6b2929ed43ce
Unni Kohonen <unni.kohonen@aalto.fi>
2022-12-22 11:27:24 +0200

    Add a root resource with annif version to rest api

--- annif/rest.py
+++ annif/rest.py
@@ -3,6 +3,8 @@ methods defined in the Swagger specification."""
 
 import connexion
 
+import importlib
+
 import annif.registry
 from annif.corpus import Document, DocumentList, SubjectSet
 from annif.exception import AnnifException
@@ -27,6 +29,15 @@ def server_error(err):
     return connexion.problem(
         status=503, title="Service unavailable", detail=err.format_message()
     )
+
+
+def show_info():
+    """return version of annif and a title for the api according to Swager spec"""
+
+    return {
+        "title": "Annif REST API",
+        "version": importlib.metadata.version("annif")
+    }
 
 
 def list_projects():
