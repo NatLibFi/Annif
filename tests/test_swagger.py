6fc315763a395dff86420f3d1f07e3b3fe3ca869
Juho Inkinen <juho.inkinen@helsinki.fi>
2023-03-14 16:39:45 +0200

    Speed up test by setting max_examples=10

--- tests/test_swagger.py
+++ tests/test_swagger.py
@@ -1,10 +1,12 @@
 """Unit tests for Annif REST API / Swagger spec"""
 import schemathesis
+from hypothesis import settings
 
 schema = schemathesis.from_path("annif/openapi/annif.yaml")
 
 
 @schema.parametrize()
+@settings(max_examples=10)
 def test_api(case, app):
     response = case.call_wsgi(app)
     case.validate_response(response)
