9d1578ea9fcbc99fca5994411a2a4a668001357a
Juho Inkinen <juho.inkinen@helsinki.fi>
2023-03-15 10:30:19 +0200

    Add custom Schemathesis check for CORS support

--- tests/test_openapi.py
+++ tests/test_openapi.py
@@ -5,8 +5,13 @@ from hypothesis import settings
 schema = schemathesis.from_path("annif/openapi/annif.yaml")
 
 
+@schemathesis.check
+def check_cors(response, case):
+    assert response.headers["access-control-allow-origin"] == "*"
+
+
 @schema.parametrize()
 @settings(max_examples=10)
 def test_api(case, app):
     response = case.call_wsgi(app)
-    case.validate_response(response)
+    case.validate_response(response, additional_checks=(check_cors,))
