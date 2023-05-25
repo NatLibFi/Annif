b74d869c7034303e829548144a4b22844fdb376a
Juho Inkinen <34240031+juhoinkinen@users.noreply.github.com>
2023-05-25 15:50:54 +0300

    Annotate (manually) annif/backend/hyperopt.py

--- annif/backend/hyperopt.py
+++ annif/backend/hyperopt.py
@@ -1,13 +1,22 @@
 """Hyperparameter optimization functionality for backends"""
+from __future__ import annotations
 
 import abc
 import collections
 import warnings
+from typing import TYPE_CHECKING, Callable, Dict, Optional
 
 import optuna
 import optuna.exceptions
 
 from .backend import AnnifBackend
+
+if TYPE_CHECKING:
+    from click.utils import LazyFile
+    from optuna.study.study import Study
+    from optuna.trial import Trial
+
+    from annif.corpus.document import DocumentCorpus
 
 HPRecommendation = collections.namedtuple("HPRecommendation", "lines score")
 
@@ -16,12 +25,12 @@ class TrialWriter:
     """Object that writes hyperparameter optimization trial results into a
     TSV file."""
 
-    def __init__(self, results_file, normalize_func):
+    def __init__(self, results_file: LazyFile, normalize_func: Callable) -> None:
         self.results_file = results_file
         self.normalize_func = normalize_func
         self.header_written = False
 
-    def write(self, study, trial):
+    def write(self, study: Study, trial: Trial) -> None:
         """Write the results of one trial into the results file.  On the
         first run, write the header line first."""
 
@@ -44,12 +53,14 @@ class TrialWriter:
 class HyperparameterOptimizer:
     """Base class for hyperparameter optimizers"""
 
-    def __init__(self, backend, corpus, metric):
+    def __init__(
+        self, backend: AnnifBackend, corpus: DocumentCorpus, metric: str
+    ) -> None:
         self._backend = backend
         self._corpus = corpus
         self._metric = metric
 
-    def _prepare(self, n_jobs=1):
+    def _prepare(self, n_jobs: int = 1):
         """Prepare the optimizer for hyperparameter evaluation.  Up to
         n_jobs parallel threads or processes may be used during the
         operation."""
@@ -57,21 +68,23 @@ class HyperparameterOptimizer:
         pass  # pragma: no cover
 
     @abc.abstractmethod
-    def _objective(self, trial):
+    def _objective(self, trial: Trial) -> float:
         """Objective function to optimize"""
         pass  # pragma: no cover
 
     @abc.abstractmethod
-    def _postprocess(self, study):
+    def _postprocess(self, study: Study) -> HPRecommendation:
         """Convert the study results into hyperparameter recommendations"""
         pass  # pragma: no cover
 
-    def _normalize(self, hps):
+    def _normalize(self, hps: Dict[str, float]) -> Dict[str, float]:
         """Normalize the given raw hyperparameters. Intended to be overridden
         by subclasses when necessary. The default is to keep them as-is."""
         return hps
 
-    def optimize(self, n_trials, n_jobs, results_file):
+    def optimize(
+        self, n_trials: int, n_jobs: int, results_file: Optional[LazyFile]
+    ) -> HPRecommendation:
         """Find the optimal hyperparameters by testing up to the given number
         of hyperparameter combinations"""
 
@@ -103,7 +116,7 @@ class AnnifHyperoptBackend(AnnifBackend):
     optimization"""
 
     @abc.abstractmethod
-    def get_hp_optimizer(self, corpus, metric):
+    def get_hp_optimizer(self, corpus: DocumentCorpus):
         """Get a HyperparameterOptimizer object that can look for
         optimal hyperparameter combinations for the given corpus,
         measured using the given metric"""
