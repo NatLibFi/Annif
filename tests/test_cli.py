f4c28dc09cfdf690567d9c580b5c5e02974a306b
Juho Inkinen <34240031+juhoinkinen@users.noreply.github.com>
2024-04-05 15:52:31 +0300

    Adapt tests for removed "annif routes" and customized "annif run" command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1055,21 +1055,7 @@ def test_run():
     result = runner.invoke(annif.cli.cli, ["run", "--help"])
     assert not result.exception
     assert result.exit_code == 0
-    assert "Run a local development server." in result.output
-
-
-def test_routes_with_flask_app():
-    # When using plain Flask only the static endpoint exists
-    result = runner.invoke(annif.cli.cli, ["routes"])
-    assert re.search(r"static\s+GET\s+\/static\/\<path:filename\>", result.output)
-    assert not re.search(r"app.home\s+GET\s+\/", result.output)
-
-
-def test_routes_with_connexion_app():
-    # When using Connexion all endpoints exist
-    result = os.popen("python annif/cli.py routes").read()
-    assert re.search(r"static\s+GET\s+\/static\/<path:filename>", result)
-    assert re.search(r"app.home\s+GET\s+\/", result)
+    assert "Run Annif in server mode for development." in result.output
 
 
 def test_completion_script_generation():
