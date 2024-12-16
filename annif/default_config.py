1675f4fbdf9889fefaaa7c3040177e0c86b14ce5
Juho Inkinen <34240031+juhoinkinen@users.noreply.github.com>
2024-12-16 15:14:32 +0200

    Resolve 413 Errors by disabling Flask's MAX_FORM_MEMORY_SIZE limit

--- annif/default_config.py
+++ annif/default_config.py
@@ -14,6 +14,7 @@ class Config(object):
     PROJECTS_CONFIG_PATH = os.environ.get("ANNIF_PROJECTS", default="")
     DATADIR = os.environ.get("ANNIF_DATADIR", default="data")
     INITIALIZE_PROJECTS = False
+    MAX_FORM_MEMORY_SIZE = None  # Disable Flask's limit
 
 
 class ProductionConfig(Config):
