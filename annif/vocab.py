d4533d9b878f00fd2ccf4a9dce416d497efa63bd
Osma Suominen <osma.suominen@helsinki.fi>
2025-03-27 11:21:20 +0200

    add no coverage pragmas for abstract methods (duh)

--- annif/vocab.py
+++ annif/vocab.py
@@ -169,38 +169,38 @@ class SubjectIndex(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def __len__(self) -> int:
-        pass
+        pass  # pragma: no cover
 
     @property
     @abc.abstractmethod
     def languages(self) -> list[str] | None:
-        pass
+        pass  # pragma: no cover
 
     @abc.abstractmethod
     def __getitem__(self, subject_id: int) -> Subject:
-        pass
+        pass  # pragma: no cover
 
     @abc.abstractmethod
     def contains_uri(self, uri: str) -> bool:
-        pass
+        pass  # pragma: no cover
 
     @abc.abstractmethod
     def by_uri(self, uri: str, warnings: bool = True) -> int | None:
         """return the subject ID of a subject by its URI, or None if not found.
         If warnings=True, log a warning message if the URI cannot be found."""
-        pass
+        pass  # pragma: no cover
 
     @abc.abstractmethod
     def by_label(self, label: str | None, language: str) -> int | None:
         """return the subject ID of a subject by its label in a given
         language"""
-        pass
+        pass  # pragma: no cover
 
     @abc.abstractmethod
     def active(self) -> list[tuple[int, Subject]]:
         """return a list of (subject_id, Subject) tuples of all subjects that
         are available for use"""
-        pass
+        pass  # pragma: no cover
 
 
 class SubjectIndexFile(SubjectIndex):
