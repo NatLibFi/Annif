053e30446c7f2a8ccb3510f463c30292c13e127a
Juho Inkinen <34240031+juhoinkinen@users.noreply.github.com>
2025-05-02 17:59:20 +0300

    Make more parameters configurable & set defaults for them

--- annif/backend/llm.py
+++ annif/backend/llm.py
@@ -28,20 +28,40 @@ if TYPE_CHECKING:
 class BaseLLMBackend(backend.AnnifBackend):
     # """Base class for TODO backends"""
 
+    DEFAULT_PARAMETERS = {
+        "api_version": "2024-02-15-preview",
+        "temperature": 0.0,
+        "top_p": 1.0,
+        "seed": 0,
+    }
+
     def initialize(self, parallel: bool = False) -> None:
-        params = self._get_backend_params(None)
         super().initialize(parallel)
         self.client = AzureOpenAI(
-            azure_endpoint=params["endpoint"],
+            azure_endpoint=self.params["endpoint"],
+            api_version=self.params["api_version"],
             api_key=os.getenv("AZURE_OPENAI_KEY"),
-            api_version="2024-02-15-preview",
         )
+        # TODO: Verify the connection?
+
+    def default_params(self):
+        params = backend.AnnifBackend.DEFAULT_PARAMETERS.copy()
+        params.update(BaseLLMBackend.DEFAULT_PARAMETERS.copy())
+        params.update(self.DEFAULT_PARAMETERS)
+        return params
 
 
 class LLMEnsembleBackend(BaseLLMBackend, ensemble.BaseEnsembleBackend):
     # """TODO backend that combines results from multiple projects"""
 
     name = "llm_ensemble"
+
+    DEFAULT_PARAMETERS = {
+        "max_prompt_tokens": 127000,
+        "llm_weight": 0.7,
+        "labels_language": "en",
+        "sources_limit": 10,
+    }
 
     system_prompt = """
         You will be given text and a list of keywords to describe it. Your task is to
@@ -68,7 +88,7 @@ class LLMEnsembleBackend(BaseLLMBackend, ensemble.BaseEnsembleBackend):
 
         batch_by_source = self._suggest_with_sources(texts, sources)
         merged_source_batch = self._merge_source_batches(
-            batch_by_source, sources, params
+            batch_by_source, sources, {"limit": params["sources_limit"]}
         )
 
         # Score the suggestion labels with the LLM
@@ -86,17 +106,20 @@ class LLMEnsembleBackend(BaseLLMBackend, ensemble.BaseEnsembleBackend):
         suggestion_batch: SuggestionBatch,
         params: dict[str, Any],
     ) -> SuggestionBatch:
+
         model = params["model"]
         encoding = tiktoken.encoding_for_model(model.rsplit("-", 1)[0])
+        max_prompt_tokens = int(params["max_prompt_tokens"])
+
         labels_batch = self._get_labels_batch(suggestion_batch)
 
         llm_batch_suggestions = []
         for text, labels in zip(texts, labels_batch):
             prompt = "Here are the keywords:\n" + "\n".join(labels) + "\n" * 3
-            text = self._truncate_text(text, encoding)
+            text = self._truncate_text(text, encoding, max_prompt_tokens)
             prompt += "Here is the text:\n" + text + "\n"
 
-            response = self._call_llm(prompt, model)
+            response = self._call_llm(prompt, model, params)
             try:
                 llm_result = json.loads(response)
             except (TypeError, json.decoder.JSONDecodeError) as err:
@@ -105,7 +128,9 @@ class LLMEnsembleBackend(BaseLLMBackend, ensemble.BaseEnsembleBackend):
                 continue  # TODO: handle this error
             llm_suggestions = [
                 SubjectSuggestion(
-                    subject_id=self.project.subjects.by_label(llm_label, "en"),
+                    subject_id=self.project.subjects.by_label(
+                        llm_label, self.params["labels_language"]
+                    ),
                     score=score,
                 )
                 for llm_label, score in llm_result.items()
@@ -120,22 +145,24 @@ class LLMEnsembleBackend(BaseLLMBackend, ensemble.BaseEnsembleBackend):
         return [
             [
                 self.project.subjects[suggestion.subject_id].labels[
-                    "en"
-                ]  # TODO: make language selectable
+                    self.params["labels_language"]
+                ]
                 for suggestion in suggestion_result
             ]
             for suggestion_result in suggestion_batch
         ]
 
-    def _truncate_text(self, text, encoding):
-        """truncate text so it contains at most MAX_PROMPT_TOKENS according to the
+    def _truncate_text(self, text, encoding, max_prompt_tokens):
+        """truncate text so it contains at most max_prompt_tokens according to the
         OpenAI tokenizer"""
-
-        MAX_PROMPT_TOKENS = 14000
         tokens = encoding.encode(text)
-        return encoding.decode(tokens[:MAX_PROMPT_TOKENS])
+        return encoding.decode(tokens[:max_prompt_tokens])
 
-    def _call_llm(self, prompt: str, model: str):
+    def _call_llm(self, prompt: str, model: str, params: dict[str, Any]) -> str:
+        temperature = float(params["temperature"])
+        top_p = float(params["top_p"])
+        seed = int(params["seed"])
+
         messages = [
             {"role": "system", "content": self.system_prompt},
             {"role": "user", "content": prompt},
@@ -144,13 +171,9 @@ class LLMEnsembleBackend(BaseLLMBackend, ensemble.BaseEnsembleBackend):
             completion = self.client.chat.completions.create(
                 model=model,
                 messages=messages,
-                temperature=0.0,
-                seed=0,
-                max_tokens=1800,
-                top_p=0.95,
-                frequency_penalty=0,
-                presence_penalty=0,
-                stop=None,
+                temperature=temperature,
+                seed=seed,
+                top_p=top_p,
                 response_format={"type": "json_object"},
             )
 
