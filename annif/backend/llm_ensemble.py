e71b9c9d5865e96ffc0488fd62121fbe03b07f76
Juho Inkinen <34240031+juhoinkinen@users.noreply.github.com>
2025-05-22 16:11:00 +0300

    Use log distribution for llm_exponent sampling in hyperopt

--- annif/backend/llm_ensemble.py
+++ annif/backend/llm_ensemble.py
@@ -308,7 +308,7 @@ class LLMEnsembleOptimizer(ensemble.EnsembleOptimizer):
         eval_batch = annif.eval.EvaluationBatch(self._backend.project.subjects)
         params = {
             "llm_weight": trial.suggest_float("llm_weight", 0.0, 1.0),
-            "llm_exponent": trial.suggest_float("llm_exponent", 0.0, 30.0),
+            "llm_exponent": trial.suggest_float("llm_exponent", 0.01, 30.0, log=True),
         }
         for merged_source_batch, llm_batch, gold_batch in zip(
             self._merged_source_batches, self._llm_batches, self._gold_batches
