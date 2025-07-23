f5b7ebfefd0869659abaa6e155e4b29b354bffb1
Osma Suominen <osma.suominen@helsinki.fi>
2025-07-23 13:42:38 +0300

    refactor kwargs_to_exclude_uris to reduce(?) cognitive complexity

--- annif/vocab/rules.py
+++ annif/vocab/rules.py
@@ -18,39 +18,51 @@ def uris_by_collection(graph: Graph, type: str) -> list[str]:
     return [str(uri) for uri in graph.objects(URIRef(type), SKOS.member)]
 
 
+def add_uris(
+    graph: Graph, uris_func: callable, uris_set: set[str], vals: list[str]
+) -> None:
+    for val in vals:
+        uris_set.update(uris_func(graph, val))
+
+
+def remove_uris(
+    graph: Graph, uris_func: callable, uris_set: set[str], vals: list[str]
+) -> None:
+    for val in vals:
+        for uri in uris_func(graph, val):
+            uris_set.discard(uri)
+
+
 def kwargs_to_exclude_uris(graph: Graph, kwargs: dict[str, str]) -> set[str]:
     exclude_uris = set()
+    actions = {
+        "exclude": lambda vals: exclude_uris.update(
+            vals if "*" not in vals else uris_by_type(graph, SKOS.Concept)
+        ),
+        "exclude_type": lambda vals: add_uris(graph, uris_by_type, exclude_uris, vals),
+        "exclude_scheme": lambda vals: add_uris(
+            graph, uris_by_scheme, exclude_uris, vals
+        ),
+        "exclude_collection": lambda vals: add_uris(
+            graph, uris_by_collection, exclude_uris, vals
+        ),
+        "include": lambda vals: exclude_uris.difference_update(vals),
+        "include_type": lambda vals: remove_uris(
+            graph, uris_by_type, exclude_uris, vals
+        ),
+        "include_scheme": lambda vals: remove_uris(
+            graph, uris_by_scheme, exclude_uris, vals
+        ),
+        "include_collection": lambda vals: remove_uris(
+            graph, uris_by_collection, exclude_uris, vals
+        ),
+    }
+
     for key, value in kwargs.items():
         vals = value.split("|")
-        if key == "exclude":
-            if "*" in vals:
-                exclude_uris.update(uris_by_type(graph, SKOS.Concept))
-            else:
-                exclude_uris.update(vals)
-        elif key == "exclude_type":
-            for val in vals:
-                exclude_uris.update(uris_by_type(graph, val))
-        elif key == "exclude_scheme":
-            for val in vals:
-                exclude_uris.update(uris_by_scheme(graph, val))
-        elif key == "exclude_collection":
-            for val in vals:
-                exclude_uris.update(uris_by_collection(graph, val))
-        elif key == "include":
-            for val in vals:
-                exclude_uris.remove(val)
-        elif key == "include_type":
-            for val in vals:
-                for uri in uris_by_type(graph, val):
-                    exclude_uris.remove(uri)
-        elif key == "include_scheme":
-            for val in vals:
-                for uri in uris_by_scheme(graph, val):
-                    exclude_uris.remove(uri)
-        elif key == "include_collection":
-            for val in vals:
-                for uri in uris_by_collection(graph, val):
-                    exclude_uris.remove(uri)
+        if key in actions:
+            actions[key](vals)
         else:
             raise ConfigurationException(f"unknown vocab keyword argument {key}")
+
     return exclude_uris
